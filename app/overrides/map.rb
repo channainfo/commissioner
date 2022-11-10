73b1e93bb10de946e0413f212e6ed442b9cbeac7
kimsrung <kimsrung.lov@gmail.com>
2022-11-10 15:00:01 +0700

    update stock location form to use .deface pattern

--- app/overrides/map.rb
+++
@@ -1,7 +0,0 @@
-# head
-Deface::Override.new(
-  name: "shared-map-map-head",
-  virtual_path: "spree/layouts/admin",
-  insert_bottom: "[data-hook='admin_inside_head']",
-  partial: "shared/map/map_head"
-)
