b7c0b936f0ce1c05935cce9db4439c37359f9d30
kimsrung <kimsrung.lov@gmail.com>
2022-11-10 15:32:58 +0700

    add vendor logo request spec

---
+++ spec/request/spree/api/v2/vendors_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe 'API V2 Storefront Vendor Spec', type: :request do
+
+  let!(:vendor_logo) { create(:vendor_logo) }
+  let!(:vendor) { create(:active_vendor, name: 'vendor', logo: vendor_logo) }
+
+  describe 'vendors#show' do
+    context 'with logo included' do
+      before { get "/api/v2/storefront/vendors/#{vendor.slug}?include=logo" }
+
+      it_behaves_like 'returns 200 HTTP status'
+
+      it 'returns logo information' do
+        json_response = JSON.parse(response.body)
+
+        expect(json_response.keys).to contain_exactly('data', 'included')
+
+        expect(json_response['included'].first['id']).to eq(vendor_logo.id.to_s)
+        expect(json_response['included'].first['attributes']['styles'].length).to eq(2)
+      end
+    end
+  end
+end
