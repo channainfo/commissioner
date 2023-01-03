46d8a40ef9ce3733e96768aa5cc3b21b8cde9610
Thea Choem <theacheng.g6@gmail.com>
2023-01-03 18:48:01 +0700

    close #90 fix couldn't call column_names on setup

--- app/models/concerns/spree_cm_commissioner/product_type.rb
+++ app/models/concerns/spree_cm_commissioner/product_type.rb
@@ -8,8 +8,8 @@ module SpreeCmCommissioner
     PRODUCT_TYPES = %i[accommodation service ecommerce]
 
     included do
-      enum product_type: PRODUCT_TYPES if column_names.include?("product_type")
-      enum primary_product_type: PRODUCT_TYPES if column_names.include?("primary_product_type")
+      enum product_type: PRODUCT_TYPES if table_exists? && column_names.include?("product_type")
+      enum primary_product_type: PRODUCT_TYPES if table_exists? && column_names.include?("primary_product_type")
     end
   end
 end
