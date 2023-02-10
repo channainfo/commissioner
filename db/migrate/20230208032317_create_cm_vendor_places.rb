83520608ca0769c2f187c057f78c7ba778887689
kimsrung <kimsrung.lov@gmail.com>
2023-02-10 09:08:53 +0000

    add spec

--- db/migrate/20230208032317_create_cm_vendor_places.rb
+++ db/migrate/20230208032317_create_cm_vendor_places.rb
@@ -3,7 +3,7 @@ class CreateCmVendorPlaces < ActiveRecord::Migration[7.0]
     create_table :cm_vendor_places, if_not_exists: true do |t|
       t.references :vendor
       t.references :place
-      t.decimal :distance, precision: 10, scale: 8
+      t.float :distance
       t.integer :position
       t.index [:vendor_id, :place_id], name: 'vendor_place'
 
