fa019fa17e0103355c3b940dd0c3a330304c09c1
Thea Choem <theacheng.g6@gmail.com>
2023-03-06 16:19:29 +0700

    close #221 add payment icons

--- app/controllers/spree/admin/vectors/icons_controller.rb
+++ app/controllers/spree/admin/vectors/icons_controller.rb
@@ -2,18 +2,23 @@ module Spree
   module Admin
     module Vectors
       class IconsController < Spree::Admin::ResourceController
-        include SpreeCmCommissioner::IconSetConcern
-
         # @overrided
         def collection
-          @collection ||= [
-            backend_icon_objects,
-            commissioner_icon_objects
-          ]
+          @collection ||= searcher.page(params[:page]).per(params[:per_page])
         end
 
         def model_class
           SpreeCmCommissioner::VectorIcon
+        end
+
+        private
+
+        def searcher
+          model_class.search(
+            path_prefix: params[:path_prefix],
+            extension: params[:extension],
+            query: params[:query]
+          )
         end
       end
     end
