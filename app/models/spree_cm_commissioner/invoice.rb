339af615abfa59d5b4c31074fdf723c5671cb6f9
panhachom <panhachom@gmail.com>
2023-03-27 14:33:27 +0700

    Close 271-invoice-printing

--- app/models/spree_cm_commissioner/invoice.rb
+++ app/models/spree_cm_commissioner/invoice.rb
@@ -4,6 +4,6 @@ module SpreeCmCommissioner
   class Invoice < ApplicationRecord
     belongs_to :order, class_name: 'Spree::Order', dependent: :destroy
     belongs_to :vendor, class_name: 'Spree::Vendor', dependent: :destroy
-    validates :invoice_number, presence: true, uniqueness: true
+    validates :invoice_number, presence: true, uniqueness: { scope: :vendor_id }
   end
 end
