e4dee332c6c800fb95a29dbdf1cb6a526994d773
kimsrung <kimsrung.lov@gmail.com>
2023-04-03 10:35:22 +0700

    Close #286 Booking Email template

--- app/assets/config/spree_cm_commissioner_manifest.js
+++ app/assets/config/spree_cm_commissioner_manifest.js
@@ -1,1 +1,2 @@
 //= link_tree ../images
+//= link spree_cm_commissioner/mailer/base.css
---
+++ app/helpers/spree/mail_helper_decorator.rb
@@ -0,0 +1,33 @@
+module Spree
+  module MailHelperDecorator
+    def current_user
+      @current_user ||= @order.user
+    end
+
+    def vendor(vendor_id)
+      Spree::Vendor.find(vendor_id)
+    end
+
+    def vendor_rating_stars(vendor)
+      rating_stars = vendor.star_rating.to_i
+      blank_stars = 5 - rating_stars
+      rating_html = ''
+
+      rating_stars.times.each do
+        rating_html += "<i class='fa-solid fa-star rating'></i>"
+      end
+
+      blank_stars.times.each do
+        rating_html += "<i class='fa-solid fa-star'></i>"
+      end
+
+      rating_html
+    end
+
+    def format_date(day)
+      day.strftime('%A %d, %B, %Y')
+    end
+  end
+end
+
+Spree::MailHelper.prepend(Spree::MailHelperDecorator)
