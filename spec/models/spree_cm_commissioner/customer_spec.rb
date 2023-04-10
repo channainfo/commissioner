9c4ccdca735dcbe07fa80d6b7a643dc114520a54
tk <tykeaboyloy@gmail.com>
2023-04-10 12:12:02 +0700

    close #314 only product's variant that has the same taxon as customer is being listed

--- spec/models/spree_cm_commissioner/customer_spec.rb
+++ spec/models/spree_cm_commissioner/customer_spec.rb
@@ -31,4 +31,40 @@ RSpec.describe SpreeCmCommissioner::Customer, type: :model do
     end
 
   end
+
+  describe '#subscribable_variants' do
+    let(:vendor) { create(:vendor) }
+
+    let(:taxon1) { create(:taxon) }
+    let(:taxon2) { create(:taxon) }
+
+    let(:customer1) { create(:cm_customer, taxon_id: taxon1.id, vendor: vendor) }
+    let(:customer2) { create(:cm_customer, taxon_id: taxon2.id, vendor: vendor) }
+
+    let(:product1) { create(:product, vendor: customer1.vendor,subscribable: true) }
+    let(:product2) { create(:product, vendor: customer2.vendor,subscribable: true) }
+
+    it "return all product's variants that has the same taxon as customer" do
+
+      product1.taxons = [taxon1]
+      product2.taxons = [taxon2]
+
+      variant11 = create(:variant, product: product1)
+      variant12 = create(:variant, product: product1)
+      variant21 = create(:variant, product: product2)
+      variant22 = create(:variant, product: product2)
+
+      variants_list1 = customer1.subscribable_variants
+      variants_list2 = customer2.subscribable_variants
+
+      expect(variants_list1.size).to eq 2
+      expect(variants_list2.size).to eq 2
+
+      expect(variants_list1).to include(variant11)
+      expect(variants_list1).to include(variant12)
+
+      expect(variants_list2).to include(variant21)
+      expect(variants_list2).to include(variant22)
+    end
+  end
 end
