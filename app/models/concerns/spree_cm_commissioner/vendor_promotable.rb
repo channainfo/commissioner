32898293d03d35bd14461259937216f975abe3a0
Thea Choem <29684683+theachoem@users.noreply.github.com>
2023-05-19 13:44:39 +0700

    close #377 active promotion to vendor-serializer (#378)

--- app/models/concerns/spree_cm_commissioner/vendor_promotable.rb
+++ app/models/concerns/spree_cm_commissioner/vendor_promotable.rb
@@ -5,6 +5,12 @@ module SpreeCmCommissioner
     included do
       has_many :promotion_rules, class_name: 'Spree::PromotionRule'
       has_many :promotions, through: :promotion_rules, class_name: 'Spree::Promotion'
+
+      has_many :active_promotions, -> { active },
+               through: :promotion_rules,
+               class_name: 'Spree::Promotion',
+               source: :promotion
+
       has_many :possible_promotions, -> { advertised.active },
                through: :promotion_rules,
                class_name: 'Spree::Promotion',
--- spec/serializers/spree/v2/storefront/vendor_serializer_spec.rb
+++ spec/serializers/spree/v2/storefront/vendor_serializer_spec.rb
@@ -2,55 +2,49 @@
 
 RSpec.describe Spree::V2::Storefront::VendorSerializer, type: :serializer do
   describe '.serializable_hash' do
-    let(:logo) { create(:vendor_logo) }
-    let(:photo1) { create(:vendor_photo) }
-    let(:vendor) { create(:vendor, logo: logo, photos: [ photo1 ]) }
-    let(:stock_location1) { create(:cm_stock_location, vendor: vendor) }
-    let(:place) { create(:cm_place) }
-    let!(:nearby_place) { create(:cm_vendor_place, vendor: vendor, place: place) }
+    let(:vendor) { create(:vendor) }
 
-    context 'with no include' do
-      subject { described_class.new(vendor).serializable_hash }
+    subject {
+      described_class.new(vendor, include: [
+        :image,
+        :logo,
+        :photos,
+        :products,
+        :stock_locations,
+        :nearby_places,
+        :places
+      ]).serializable_hash
+    }
 
-      it { expect(subject[:data][:attributes]).to include(:about_us) }
-      it { expect(subject[:data][:attributes]).to include(:contact_us) }
-      it { expect(subject[:data][:attributes]).to include(:name) }
-      it { expect(subject[:data][:attributes]).to include(:slug) }
-      it { expect(subject[:data][:attributes]).to include(:min_price) }
-      it { expect(subject[:data][:attributes]).to include(:max_price) }
-      it { expect(subject[:data][:attributes]).to include(:star_rating) }
-      it { expect(subject[:data][:attributes]).to include(:short_description) }
-      it { expect(subject[:data][:relationships]).to include(:image) }
-      it { expect(subject[:data][:relationships]).to include(:logo) }
-      it { expect(subject[:data][:relationships]).to include(:photos) }
-      it { expect(subject[:data][:relationships]).to include(:products) }
-      it { expect(subject[:data][:relationships]).to include(:stock_locations) }
-      it { expect(subject[:data][:relationships]).to include(:vendor_kind_option_types) }
-      it { expect(subject[:data][:relationships]).to include(:vendor_kind_option_values) }
-      it { expect(subject[:data][:relationships]).to include(:promoted_option_types) }
-      it { expect(subject[:data][:relationships]).to include(:nearby_places) }
-      it { expect(subject[:data][:relationships]).to include(:places) }
-      it { expect(subject[:data][:relationships]).to include(:promoted_option_values) }
+    it 'returns exact vendor attributes' do
+      expect(subject[:data][:attributes].keys).to contain_exactly(
+        :name,
+        :about_us,
+        :slug,
+        :contact_us,
+        :min_price,
+        :max_price,
+        :star_rating,
+        :short_description
+      )
     end
 
-    context 'with include' do
-      subject {
-        described_class.new(vendor, include: [
-          :image,
-          :logo,
-          :photos,
-          :products,
-          :stock_locations,
-          :nearby_places,
-          :places
-        ]).serializable_hash
-      }
-
-      it { expect(subject[:included].select {|e| e[:type] == :vendor_logo}.size).to eq 1}
-      it { expect(subject[:included].select {|e| e[:type] == :vendor_photo}.size).to eq 1}
-      it { expect(subject[:included].select {|e| e[:type] == :stock_location}.size).to eq 1}
-      it { expect(subject[:included].select {|e| e[:type] == :nearby_place}.size).to eq 1}
-      it { expect(subject[:included].select {|e| e[:type] == :place}.size).to eq 1}
+    it 'returns exact vendor relationships' do
+      expect(subject[:data][:relationships].keys).to contain_exactly(
+        :image,
+        :products,
+        :stock_locations,
+        :variants,
+        :photos,
+        :vendor_kind_option_types,
+        :promoted_option_types,
+        :nearby_places,
+        :places,
+        :promoted_option_values,
+        :vendor_kind_option_values,
+        :active_promotions,
+        :logo
+      )
     end
   end
 end
