a0fbfaab7c8d4aadd47f2a905902439d3789627f
panhachom <panhachom@gmail.com>
2023-06-14 15:11:46 +0700

    Close #418-add-device_type-to-CmDeviceToken

--- app/serializers/spree/v2/storefront/user_device_token_serializer.rb
+++ app/serializers/spree/v2/storefront/user_device_token_serializer.rb
@@ -2,7 +2,7 @@ module Spree
   module V2
     module Storefront
       class UserDeviceTokenSerializer < BaseSerializer
-        attributes :user_id, :registration_token, :client_name, :client_version, :meta
+        attributes :user_id, :registration_token, :client_name, :client_version, :meta, :device_type
       end
     end
   end
--- app/serializers/spree/v2/storefront/user_serializer_decorator.rb
+++ app/serializers/spree/v2/storefront/user_serializer_decorator.rb
@@ -5,6 +5,7 @@ module Spree
         def self.prepended(base)
           base.attributes :first_name, :last_name, :phone_number, :intel_phone_number, :country_code
           base.has_one :profile, serializer: :user_profile
+          base.has_many :device_tokens, serializer: Spree::V2::Storefront::UserDeviceTokenSerializer
         end
       end
     end
