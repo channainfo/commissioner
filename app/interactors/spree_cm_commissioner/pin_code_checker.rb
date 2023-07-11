56864d7adf9ca718e47923dbec4072ac75a03d22
panhachom <panhachom@gmail.com>
2023-07-11 09:43:37 +0700

    Close #465 pin code checker api

---
+++ app/interactors/spree_cm_commissioner/pin_code_checker.rb
@@ -0,0 +1,17 @@
+module SpreeCmCommissioner
+  class PinCodeChecker < BaseInteractor
+    def call
+      pin_coder = context.type.constantize
+      options = context.to_h.slice(:id, :code, :email, :phone_number, :type, :long_life_pin_code, :user_validation_check, :country_code)
+      @result = pin_coder.check?(options)
+
+      return unless @result != 'ok'
+
+      context.fail!(message: readable_result)
+    end
+
+    def readable_result
+      I18n.t("pincode_checker.error_type.#{@result}")
+    end
+  end
+end
