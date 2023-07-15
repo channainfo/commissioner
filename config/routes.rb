6b2c2e5e7525cb212a529cffe6c25f41fc3a189d
panhachom <panhachom@gmail.com>
2023-07-15 10:58:38 +0700

    Close #472 pin code generator api

--- config/routes.rb
+++ config/routes.rb
@@ -142,6 +142,7 @@ Spree::Core::Engine.add_routes do
         resources :customer_notifications, only: [:show]
         resource :user_registration_with_pin_codes, only: [:create]
         resources :user_device_token_registrations, only: %i[create destroy]
+        resources :pin_code_generators, only: [:create]
         resources :pin_code_checkers, only: [:update]
         resources :vendors do
           resources :nearby_places, only: %i[index]
---
+++ spec/request/spree/api/v2/controller/pin_code_generaters_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+RSpec.describe Spree::Api::V2::Storefront::PinCodeGeneratorsController, type: :controller do
+  describe 'POST create' do
+    context 'with valid params' do
+      it 'create new pin code' do
+        set_application_token
+
+        post :create, params: { email: 'john@gmail.com' , type: 'SpreeCmCommissioner::PinCodeLogin' }
+
+        expect(response.status).to eq 201
+        expect(json_response_body['data']['attributes']['contact']).to eq 'john@gmail.com'
+        expect(json_response_body['data']['attributes']['type']).to eq 'SpreeCmCommissioner::PinCodeLogin'
+      end
+    end
+    context 'with ininvalid params' do
+      it 'return error' do
+        set_application_token
+
+        post :create, params: { email: '0964103875', type: 'SpreeCmCommissioner::PinCodeLogin' }
+
+        expect(response.status).to eq 400
+        expect(json_response_body['error']).to eq 'Invalid email address'
+      end
+    end
+  end
+end
