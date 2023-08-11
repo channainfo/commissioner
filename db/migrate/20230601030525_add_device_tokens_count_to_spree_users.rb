4580f11e8e767a5cfff8c9ce8a7facf9b6c66ce5
tk <tykeaboyloy@gmail.com>
2023-08-11 10:58:03 +0700

    close #510 reformat invoice id

--- db/migrate/20230601030525_add_device_tokens_count_to_spree_users.rb
+++ db/migrate/20230601030525_add_device_tokens_count_to_spree_users.rb
@@ -1,5 +1,5 @@
 class AddDeviceTokensCountToSpreeUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :spree_users, :device_tokens_count, :integer, default: 0
+    add_column :spree_users, :device_tokens_count, :integer, default: 0, if_not_exists: true
   end
 end
--- db/migrate/20230622043118_remove_type_from_spree_cm_commissioner_notifications.rb
+++ db/migrate/20230622043118_remove_type_from_spree_cm_commissioner_notifications.rb
@@ -1,5 +1,5 @@
 class RemoveTypeFromSpreeCmCommissionerNotifications < ActiveRecord::Migration[7.0]
   def change
-    remove_column :cm_notifications, :type, :string
+    remove_column :cm_notifications, :type, :string, if_exists: true
   end
 end
