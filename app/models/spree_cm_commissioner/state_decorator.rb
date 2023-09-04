bf5c680c05b028d407b2335da4534520669e9b4e
tk <tykeaboyloy@gmail.com>
2023-09-04 17:14:53 +0700

    close #546 can now create new location

--- app/models/spree_cm_commissioner/state_decorator.rb
+++ app/models/spree_cm_commissioner/state_decorator.rb
@@ -2,6 +2,7 @@ module SpreeCmCommissioner
   module StateDecorator
     def self.prepended(base)
       base.has_many :vendors
+      base.whitelisted_ransackable_attributes = %w[name abbr]
 
       def update_total_inventory
         update(total_inventory: vendors.pluck(:total_inventory).sum)
