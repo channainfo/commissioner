333ea065cb445b07eaf1e2a84f7e4e8b4be116e0
Thea Choem <theacheng.g6@gmail.com>
2023-09-16 12:19:55 +0700

    close #591 undefine try_spree_current_user on exception notifier

--- app/controllers/concerns/spree_cm_commissioner/exception_notificable.rb
+++ app/controllers/concerns/spree_cm_commissioner/exception_notificable.rb
@@ -11,8 +11,8 @@ module SpreeCmCommissioner
     def prepare_exception_notifier
       exception_data = {}
 
-      exception_data[:user] = [try_spree_current_user.id].compact.join('-') if try_spree_current_user.present?
-      exception_data[:order] = [spree_current_order.number].compact.join('-') if defined?(spree_current_order)
+      exception_data[:user] = [try_spree_current_user&.id].compact.join('-') if defined?(try_spree_current_user)
+      exception_data[:order] = [spree_current_order&.number].compact.join('-') if defined?(spree_current_order)
 
       request.env['exception_notifier.exception_data'] = exception_data
     end
