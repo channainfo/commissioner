e10f70810dba8eab98268ece6ae28815a22fe125
Channa Ly <channa.info@gmail.com>
2023-09-23 09:42:26 +0700

    Enhance wrong role db connection #614 (#615)

--- app/controllers/spree_cm_commissioner/admin/user_sessions_controller_decorator.rb
+++ app/controllers/spree_cm_commissioner/admin/user_sessions_controller_decorator.rb
@@ -2,9 +2,10 @@ module SpreeCmCommissioner
   module Admin
     module UserSessionsControllerDecorator
       def self.prepended(base)
-        # spree_devise_auth gem use get as destroy
-        # get '/logout' => 'user_sessions#destroy', :as => :logout
-        base.around_action :set_writing_role, only: %i[destroy]
+        # spree_devise_auth
+        # get '/logout' => #destroy uses destroy
+        # get '/login' => #ne  updates signe_in_count
+        base.around_action :set_writing_role, only: %i[destroy new]
       end
     end
   end
---
+++ app/controllers/spree_cm_commissioner/api/v2/storefront/checkout_controller_decorator.rb
@@ -0,0 +1,19 @@
+module SpreeCmCommissioner
+  module Api
+    module V2
+      module Storefront
+        module CheckoutControllerDecorator
+          def self.prepended(base)
+            # spree_vpago gem update payment state in #payment_redirect
+            # GET /payment_redirect
+            base.around_action :set_writing_role, only: %i[payment_redirect]
+          end
+        end
+      end
+    end
+  end
+end
+
+unless Spree::Api::V2::Storefront::CheckoutController.ancestors.include?(SpreeCmCommissioner::Api::V2::Storefront::CheckoutControllerDecorator)
+  Spree::Api::V2::Storefront::CheckoutController.prepend(SpreeCmCommissioner::Api::V2::Storefront::CheckoutControllerDecorator)
+end
