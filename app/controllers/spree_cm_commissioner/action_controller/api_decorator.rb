9f134a9f3c080e3c9a44be5c272bab5cc2b24a99
Thea Choem <theacheng.g6@gmail.com>
2023-09-27 17:50:03 +0700

    close #630 add application controller decorator to ActionController::API

---
+++ app/controllers/spree_cm_commissioner/action_controller/api_decorator.rb
@@ -0,0 +1,20 @@
+module SpreeCmCommissioner
+  module ActionController
+    module ApiDecorator
+      def self.prepended(base)
+        base.include SpreeCmCommissioner::ExceptionNotificable
+      end
+
+      # Annonymous block: https://www.rubydoc.info/gems/rubocop/RuboCop/Cop/Naming/BlockForwarding
+      def set_writing_role(&)
+        ActiveRecord::Base.connected_to(role: :writing, &)
+      end
+    end
+  end
+end
+
+unless ActionController::API.included_modules.include?(SpreeCmCommissioner::ActionController::ApiDecorator)
+  ActiveSupport.on_load(:action_controller) do
+    prepend SpreeCmCommissioner::ActionController::ApiDecorator
+  end
+end
