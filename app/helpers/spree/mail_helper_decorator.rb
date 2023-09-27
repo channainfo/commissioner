d5cdf62d08b3cb413143dfe519ca0dcfe52616de
kimsrung <kimsrung.lov@gmail.com>
2023-09-27 15:00:22 +0700

    Close #599 bookme+ email order confirmation (#625)
    * Close #599 bookme+ email order confirmation
    
    * fix order store url

--- app/helpers/spree/mail_helper_decorator.rb
+++ app/helpers/spree/mail_helper_decorator.rb
@@ -8,6 +8,14 @@ module Spree
       Spree::Vendor.find(vendor_id)
     end
 
+    def full_address(stock_location)
+      address = stock_location.address1 || stock_location.address2
+      address += "  #{stock_location.city}" if stock_location.city.present?
+      state_name = Spree::State.find(stock_location.state_id)&.name
+      country_name = Spree::Country.find(stock_location.country_id)&.name
+      "#{address} #{state_name}, #{country_name}"
+    end
+
     def vendor_rating_stars(vendor)
       rating_stars = vendor.star_rating.to_i
       blank_stars = 5 - rating_stars
@@ -26,6 +34,22 @@ module Spree
 
     def format_date(day)
       day.strftime('%A %d, %B, %Y')
+    end
+
+    def google_map(lat, lon)
+      "https://www.google.com/maps/@#{lat},#{lon},15z?entry=ttu"
+    end
+
+    def generate_qr(order)
+      qrcode = RQRCode::QRCode.new("#{order.store.url}/orders/#{order.number}")
+      qrcode.as_svg(
+        color: '000',
+        shape_rendering: 'crispEdges',
+        module_size: 5,
+        standalone: true,
+        use_path: true,
+        viewbox: '0 0 20 10'
+      )
     end
   end
 end
