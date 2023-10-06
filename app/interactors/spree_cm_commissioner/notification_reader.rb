0650e1f54ad998482df9a697ddc18578820ea881
Chom Panha <92453740+panhachom@users.noreply.github.com>
2023-10-06 18:55:02 +0700

    close #672 enhance mark as read api (#673)

--- app/interactors/spree_cm_commissioner/notification_reader.rb
+++ app/interactors/spree_cm_commissioner/notification_reader.rb
@@ -1,8 +1,11 @@
 module SpreeCmCommissioner
   class NotificationReader < BaseInteractor
-    delegate :notification, to: :context
+    delegate :id, to: :context
 
     def call
+      notification = SpreeCmCommissioner::Notification.find(id)
+      return context.fail!(error_message: 'Notification not found') if notification.nil?
+
       notification.mark_as_read! if notification.unread?
     end
   end
