675be7d29467ac81ae4507251d870fd52828fd94
Kimsrung Lov <kimsrung.lov@gmail.com>
2023-10-10 20:56:17 +0700

    change to use layout table

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -4,13 +4,13 @@ module Spree
       @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
       return false if @order.email.blank?
 
-      current_store = @order.store
+      @current_store = @order.store
       @product_type = @order.products.first&.product_type || 'accommodation'
 
       subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
-      subject += "#{current_store&.name} Booking Confirmation ##{@order.number}"
+      subject += "#{@current_store&.name} Booking Confirmation ##{@order.number}"
 
-      mail(to: @order.email, from: from_address, subject: subject, store_url: current_store.url) do |format|
+      mail(to: @order.email, from: from_address, subject: subject, store_url: @current_store.url) do |format|
         format.html { render layout: 'spree_cm_commissioner/layouts/order_mailer' }
         format.text
       end
