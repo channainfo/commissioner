f3689c6d5c5ce66eb2ee568fb7a78e780ad87ed5
Thea Choem <29684683+theachoem@users.noreply.github.com>
2023-10-20 14:18:29 +0700

    close #736 add full backtrace to telegram exception notifier (#737)

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -2,7 +2,6 @@ if ENV['EXCEPTION_NOTIFY_ENABLE'] == 'yes'
   Rails.application.config.middleware.use(
     ExceptionNotification::Rack,
     'spree_cm_commissioner/telegram': {
-      token: ENV.fetch('EXCEPTION_TELEGRAM_BOT_TOKEN', nil),
       channel_id: ENV.fetch('EXCEPTION_NOTIFIER_TELEGRAM_CHANNEL_ID', nil)
     }
   )
