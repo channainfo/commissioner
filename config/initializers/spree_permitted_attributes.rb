f151168a4314ca7b5594c040c116b70b35a8d106
sreyleak <sreyleak.deth19@gmail.com>
2023-10-25 10:33:04 +0000

    Close #753 make phone number values to store in the input field and get their value to display on the user screen

--- config/initializers/spree_permitted_attributes.rb
+++ config/initializers/spree_permitted_attributes.rb
@@ -2,7 +2,7 @@ module Spree
   module PermittedAttributes
     @@vendor_attributes << :logo
     @@line_item_attributes += %i[from_date to_date]
-    @@user_attributes      += %i[first_name last_name dob gender profile]
+    @@user_attributes      += %i[first_name last_name dob gender phone_number profile]
     @@taxon_attributes     += %i[category_icon custom_redirect_url to_date from_date kind]
     @@store_attributes     += %i[preferred_telegram_order_alert_chat_id preferred_telegram_order_request_alert_chat_id]
 
