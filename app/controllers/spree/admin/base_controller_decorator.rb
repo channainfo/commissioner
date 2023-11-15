e721b1115bb4265209e6f6160a269a4350853467
Thea Choem <theacheng.g6@gmail.com>
2023-11-15 11:27:50 +0700

    close #807 write query attempted while in readonly-mode spree_users

--- app/controllers/spree/admin/base_controller_decorator.rb
+++ app/controllers/spree/admin/base_controller_decorator.rb
@@ -1,6 +1,14 @@
 module Spree
   module Admin
     module BaseControllerDecorator
+      # even db in read only mode, authorizor still need to run Warden callbacks which will write to db
+      # this include update tracked fields such as sign_in_count, last_sign_in_at, etc.
+      def authorize!(*args)
+        ActiveRecord::Base.connected_to(role: :writing) do
+          super
+        end
+      end
+
       # 2023-07-31
       def parse_date!(date, format: nil)
         Date.strptime(date.to_s, format || '%Y-%m-%d')
