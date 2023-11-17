bdb05b0f28dd9444003e4c577146f0aff08caf49
Thea Choem <theacheng.g6@gmail.com>
2023-11-17 14:35:36 +0700

    close #821 fix weekend always not eligible

--- app/models/spree_cm_commissioner/promotion/rules/weekend.rb
+++ app/models/spree_cm_commissioner/promotion/rules/weekend.rb
@@ -4,11 +4,11 @@ module SpreeCmCommissioner
       class Weekend < Date
         DAYS_INTO_WEEK = DateAndTime::Calculations::DAYS_INTO_WEEK
 
-        preference :weekend_days, :array, default: [6, 0]
+        preference :weekend_days, :array, default: %w[6 0]
 
         # override
         def date_eligible?(date)
-          preferred_weekend_days.include?(date.wday)
+          preferred_weekend_days.include?(date.wday.to_s)
         end
       end
     end
