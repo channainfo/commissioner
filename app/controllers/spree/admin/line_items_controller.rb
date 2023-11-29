f3c6bd94ee155795fe2dfb85a5ac39c5e7bba915
Thornsopanha <cheangthornsopanha@gmail.com>
2023-11-29 10:57:09 +0700

    close #830 edit line item fields backend

---
+++ app/controllers/spree/admin/line_items_controller.rb
@@ -0,0 +1,40 @@
+module Spree
+  module Admin
+    class LineItemsController < Spree::Admin::ResourceController
+      before_action :load_data, only: %i[update edit]
+
+      def edit
+        return unless @line_item.nil?
+
+        flash[:alert] = Spree.t('notice_messages.line_item_not_found')
+        redirect_to cart_admin_order_path(@order)
+      end
+
+      def update
+        result = update_service.call(line_item: @line_item, line_item_attributes: permitted_resource_params)
+
+        if result.success?
+          redirect_to cart_admin_order_path(@order)
+        else
+          render :edit
+        end
+      end
+
+      private
+
+      def permitted_resource_params
+        params.require(:line_item).permit(:quantity, :from_date, :to_date)
+      end
+
+      def update_service
+        Spree::Dependencies.line_item_update_service.constantize
+      end
+
+      def load_data
+        scope = current_store.orders
+        @order = scope.find(params[:order_id])
+        @line_item = @order.line_items.find(params[:id])
+      end
+    end
+  end
+end
