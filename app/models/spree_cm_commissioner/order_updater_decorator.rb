d9970e1eb71c21b2489ae2fb2096953671a2e657
Kimsrung Lov <kimsrung.lov@gmail.com>
2023-12-11 12:43:46 +0700

    remove cap field from promotion

--- app/models/spree_cm_commissioner/order_updater_decorator.rb
+++ app/models/spree_cm_commissioner/order_updater_decorator.rb
@@ -5,23 +5,6 @@ module SpreeCmCommissioner
       order.item_total = line_items.sum(&:amount)
       update_order_total
     end
-
-    # override
-    def update_adjustment_total
-      super
-
-      promotion_adjustment = adjustments.promotion.eligible.first
-
-      return if promotion_adjustment.blank? # Check if the order has a promotion adjustment
-
-      promotion = promotion_adjustment.source.promotion
-
-      return unless promotion&.cap && order.adjustment_total.abs > promotion&.cap # Check if cap exist
-
-      order.adjustment_total = order.promo_total = promotion.cap * -1
-
-      update_order_total
-    end
   end
 end
 
--- db/migrate/20231114015730_add_cap_to_spree_promotion.rb
+++
@@ -1,5 +0,0 @@
-class AddCapToSpreePromotion < ActiveRecord::Migration[7.0]
-  def change
-    add_column :spree_promotions, :cap, :float, if_not_exists: true
-  end
-end
