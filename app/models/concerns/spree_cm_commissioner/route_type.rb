531a681196cd0683fcd188417240a1f54969b0ef
tk <tykeaboyloy@gmail.com>
2024-01-08 16:26:11 +0700

    close #976 change the term bus to automobile

--- app/models/concerns/spree_cm_commissioner/route_type.rb
+++ app/models/concerns/spree_cm_commissioner/route_type.rb
@@ -2,7 +2,7 @@ module SpreeCmCommissioner
   module RouteType
     extend ActiveSupport::Concern
 
-    ROUTE_TYPES = %i[bus subway rails ferry].freeze
+    ROUTE_TYPES = %i[automobile subway rails ferry].freeze
 
     included do
       enum route_type: ROUTE_TYPES if table_exists? && column_names.include?('route_type')
