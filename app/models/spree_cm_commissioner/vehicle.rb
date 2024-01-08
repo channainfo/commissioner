0271fe4f297b768afa0e949c2a2855726a2c3ab8
tk <tykeaboyloy@gmail.com>
2024-01-08 16:58:21 +0700

    close #980 added route_type field to vehicle

--- app/models/spree_cm_commissioner/vehicle.rb
+++ app/models/spree_cm_commissioner/vehicle.rb
@@ -2,14 +2,22 @@ require_dependency 'spree_cm_commissioner'
 
 module SpreeCmCommissioner
   class Vehicle < SpreeCmCommissioner::Base
-    belongs_to :vehicle_type, class_name: 'SpreeCmCommissioner::VehicleType', dependent: :destroy
+    include SpreeCmCommissioner::RouteType
+
+    belongs_to :vehicle_type, class_name: 'SpreeCmCommissioner::VehicleType'
     belongs_to :vendor, class_name: 'Spree::Vendor'
+
+    before_save :set_route_type, if: :vehicle_type_id_changed?
 
     has_many :vehicle_photo, class_name: 'SpreeCmCommissioner::VehiclePhoto', as: :viewable, dependent: :destroy
 
     validates :code, uniqueness: { scope: :vendor_id }, presence: true
     validates :license_plate, uniqueness: {}, allow_blank: true
     validates :vehicle_type, presence: true
+
+    def set_route_type
+      self.route_type = vehicle_type.route_type
+    end
 
     self.whitelisted_ransackable_attributes = %w[license_plate code]
     self.whitelisted_ransackable_associations = %w[vehicle_type]
