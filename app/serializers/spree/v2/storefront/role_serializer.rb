e9cd068a15a7b0c6bce8cec8fb7288ddf2e7c3d7
LengTech11 <soklenghoungofficial11@gmail.com>
2024-01-09 14:03:56 +0700

    close #983 add roles to account api

---
+++ app/serializers/spree/v2/storefront/role_serializer.rb
@@ -0,0 +1,9 @@
+module Spree
+  module V2
+    module Storefront
+      class RoleSerializer < BaseSerializer
+        attributes :name
+      end
+    end
+  end
+end
--- app/serializers/spree/v2/storefront/user_serializer_decorator.rb
+++ app/serializers/spree/v2/storefront/user_serializer_decorator.rb
@@ -6,6 +6,7 @@ module Spree
           base.attributes :first_name, :last_name, :gender, :phone_number, :intel_phone_number, :country_code
           base.has_one :profile, serializer: ::Spree::V2::Storefront::UserProfileSerializer
           base.has_many :device_tokens, serializer: Spree::V2::Storefront::UserDeviceTokenSerializer
+          base.has_many :spree_roles, serializer: Spree::V2::Storefront::RoleSerializer
         end
       end
     end
