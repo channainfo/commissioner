a6a30de1a5ab02b63a9993f6d7ddbd116e62dec3
Chheng <oeungchungchheng2205@gmail.com>
2024-01-09 16:53:01 +0700

    Close #997 Add Amenity to vehicle type

--- config/routes.rb
+++ config/routes.rb
@@ -150,6 +150,15 @@ Spree::Core::Engine.add_routes do
   end
 
   namespace :transit do
+    namespace :vectors do
+      resources :icons, only: [:index]
+      resources :amenity_values, only: %i[index update] do
+        collection do
+          post :update
+        end
+      end
+    end
+
     resources :reservations
     resources :locations
     resources :vendors do
@@ -172,6 +181,10 @@ Spree::Core::Engine.add_routes do
         end
       end
     end
+
+    resource :amenity, only: [:new, :create , :edit, :update]
+    post "/amenity/update_positions"
+    post "/amenity/update_values_positions"
 
     resources :taxons, only: %i[index show]
     resources :vehicle_types do
