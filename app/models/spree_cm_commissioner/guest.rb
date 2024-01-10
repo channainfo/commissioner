76aefbd8549004a3acbc80039c93351cb567ae68
LengTech11 <soklenghoungofficial11@gmail.com>
2024-01-10 16:05:01 +0700

    close #984 generate qr for guests

--- app/models/spree_cm_commissioner/guest.rb
+++ app/models/spree_cm_commissioner/guest.rb
@@ -6,5 +6,43 @@ module SpreeCmCommissioner
     belongs_to :occupation, class_name: 'Spree::Taxon'
 
     has_one :id_card, class_name: 'SpreeCmCommissioner::IdCard', dependent: :destroy
+
+    def generate_svg_qr
+      qrcode = RQRCode::QRCode.new(qr_data)
+      qrcode.as_svg(
+        color: '000',
+        shape_rendering: 'crispEdges',
+        module_size: 5,
+        standalone: true,
+        use_path: true,
+        viewbox: '0 0 20 10'
+      )
+    end
+
+    def generate_png_qr(size = 120)
+      qrcode = RQRCode::QRCode.new(qr_data)
+      qrcode.as_png(
+        bit_depth: 1,
+        border_modules: 1,
+        color_mode: ChunkyPNG::COLOR_GRAYSCALE,
+        color: 'black',
+        file: nil,
+        fill: 'white',
+        module_px_size: 4,
+        resize_exactly_to: false,
+        resize_gte_to: false,
+        size: size
+      )
+    end
+
+    def qr_data
+      # "#{id}-#{token}"
+      # "#{id}-#{line_item_id}-#{line_item&.order_id}"
+      if id && line_item_id && line_item&.order_id
+        "#{id}-#{line_item_id}-#{line_item.order_id}"
+      else
+        'INVALID'
+      end
+    end
   end
 end
