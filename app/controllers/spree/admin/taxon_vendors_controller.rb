c838a801712dcbce7079702b4f6745aaa30afea5
Sreyleak Deth <115885247+Sreyleak-Deth@users.noreply.github.com>
2024-01-18 04:18:10 +0000

    Close #1022 List all vendors in taxonomies (#1023)

--- app/controllers/spree/admin/taxon_vendors_controller.rb
+++ app/controllers/spree/admin/taxon_vendors_controller.rb
@@ -1,10 +1,12 @@
 module Spree
   module Admin
     class TaxonVendorsController < Spree::Admin::ResourceController
-      belongs_to 'spree/taxonomy', optional: true, find_by: :permalink
-
       def index
-        @taxon_vendors = @taxonomy.taxon_vendors
+        @taxonomy = Spree::Taxonomy.find(params[:taxonomy_id])
+        @taxon = @taxonomy.taxons.find(params[:taxon_id])
+
+        # Retrieve taxon vendors associated with the taxon
+        @taxon_vendors = @taxon.taxon_vendors
       end
 
       def model_class
@@ -12,11 +14,11 @@ module Spree
       end
 
       def object_name
-        'spree_cm_commissioner_taxon_vendors'
+        'spree_cm_commissioner_taxon_vendor'
       end
 
       def collection_url(options = {})
-        admin_taxonomy_taxon_vendors_url(options)
+        admin_taxonomy_taxon_taxon_vendors_url(taxonomy_id: @taxonomy, taxon_id: @taxon, options: options)
       end
     end
   end
