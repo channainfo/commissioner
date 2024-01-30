ece6733bef2ae6702f2f2cf66b92f18a0aeb9d4d
Vaneath <109834020+vaneath@users.noreply.github.com>
2024-01-30 18:00:17 +0700

    close #1059 update homepage_section attribute(updated_at) when its relatable is updated (#1060)

--- app/models/spree_cm_commissioner/homepage_section_relatable.rb
+++ app/models/spree_cm_commissioner/homepage_section_relatable.rb
@@ -1,10 +1,17 @@
 module SpreeCmCommissioner
   class HomepageSectionRelatable < Base
     acts_as_list column: :position
+    after_update :update_homepage_section
 
-    belongs_to :homepage_section, optional: false, inverse_of: :homepage_section_relatables, touch: true, dependent: :destroy
-    belongs_to :relatable, polymorphic: true, optional: false, inverse_of: :homepage_section_relatables, touch: true, dependent: :destroy
+    belongs_to :homepage_section, optional: false, inverse_of: :homepage_section_relatables, dependent: :destroy
+    belongs_to :relatable, polymorphic: true, optional: false, inverse_of: :homepage_section_relatables, dependent: :destroy
 
     scope :active, -> { where(active: true).order(:position) }
+
+    private
+
+    def update_homepage_section
+      homepage_section.update(updated_at: Time.zone.now)
+    end
   end
 end
