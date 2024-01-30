8bbc10056e11a8106aac8d592f308355556bd2e2
vaneath <vaneathzz@gmail.com>
2024-01-30 17:56:10 +0700

    close #1059 update homepage_section attribute(updated_at) when its relatable is updated

--- spec/models/spree_cm_commissioner/homepage_section_relatable_spec.rb
+++ spec/models/spree_cm_commissioner/homepage_section_relatable_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 RSpec.describe SpreeCmCommissioner::HomepageSectionRelatable, type: :model do
   describe 'associations' do
-    it { should belong_to(:homepage_section).inverse_of(:homepage_section_relatables).touch.dependent(:destroy) }
-    it { should belong_to(:relatable).inverse_of(:homepage_section_relatables).touch.dependent(:destroy) }
+    it { should belong_to(:homepage_section).inverse_of(:homepage_section_relatables).dependent(:destroy) }
+    it { should belong_to(:relatable).inverse_of(:homepage_section_relatables).dependent(:destroy) }
   end
 
   describe '#active' do
@@ -16,6 +16,17 @@ RSpec.describe SpreeCmCommissioner::HomepageSectionRelatable, type: :model do
       position_4 = create(:cm_homepage_section_relatable, homepage_section: section, position: 4, active: true)
 
       expect(described_class.active.to_a).to eq [position_2, position_3, position_4]
+    end
+  end
+
+  describe '#update_homepage_section' do
+    it 'should update the homepage_section updated_at when relatable is updated' do
+      homepage_section = create(:cm_homepage_section)
+      relatable = create(:cm_homepage_section_relatable, homepage_section: homepage_section, active: true)
+
+      relatable.update(active: false)
+
+      expect(homepage_section.updated_at.to_i).to eq relatable.updated_at.to_i
     end
   end
 end
