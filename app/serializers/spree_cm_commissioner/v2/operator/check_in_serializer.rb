26fed6656f17a736607cec1ad076848073f67460
LengTech11 <soklenghoungofficial11@gmail.com>
2024-02-16 12:26:43 +0700

    close #1125 return line_items to check_ins

--- app/serializers/spree_cm_commissioner/v2/operator/check_in_serializer.rb
+++ app/serializers/spree_cm_commissioner/v2/operator/check_in_serializer.rb
@@ -16,6 +16,7 @@ module SpreeCmCommissioner
 
         belongs_to :check_in_by, serializer: ::Spree::V2::Storefront::UserSerializer
         belongs_to :guest, serializer: ::SpreeCmCommissioner::V2::Operator::GuestSerializer
+        belongs_to :line_item, serializer: ::SpreeCmCommissioner::V2::Operator::LineItemSerializer
       end
     end
   end
