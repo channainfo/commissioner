307cd3d3473319608ef443cf81e5085e1b8f58e1
Vaneath <109834020+vaneath@users.noreply.github.com>
2024-03-19 11:39:07 +0700

    close #1195 export guest data to csv (#1196)

--- app/controllers/spree/events/guests_controller.rb
+++ app/controllers/spree/events/guests_controller.rb
@@ -11,16 +11,35 @@ module Spree
         SpreeCmCommissioner::Guest
       end
 
+      def index
+        file_name = Rails.root.join('tmp', csv_name)
+
+        respond_with(collection) do |format|
+          format.csv do
+            SpreeCmCommissioner::GenerateGuestsCsv.call(
+              collection: collection,
+              file_name: file_name
+            )
+
+            send_file file_name
+          end
+        end
+      end
+
       def edit
         @guest = SpreeCmCommissioner::Guest.find(params[:id])
         @event = @guest.event
       end
 
+      private
+
+      def csv_name
+        @csv_name ||= "guests-data-#{current_event.name.downcase.gsub(' ', '-')}-#{Time.current.to_i}.csv"
+      end
+
       def permitted_resource_params
         params.required(:spree_cm_commissioner_guest).permit(:entry_type)
       end
-
-      protected
 
       def collection
         scope = model_class.where(event_id: current_event.id)
