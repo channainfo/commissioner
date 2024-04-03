c816d70ec108fb975c70308652bc7a995de04cac
Thea Choem <29684683+theachoem@users.noreply.github.com>
2024-04-03 14:29:17 +0700

    Revert "close #1216 allow Spree::Price to belong to polymophic instead of variant"

--- spec/models/spree/price_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe Spree::Price, type: :model do
-  describe 'associations' do
-    it { should belong_to(:priceable).required(true) }
-    it { should belong_to(:variant).required(false) }
-  end
-end
