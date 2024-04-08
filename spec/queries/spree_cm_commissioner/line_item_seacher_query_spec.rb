1e9dec804f935b6672ee5a005c99a9f2072d583f
LengTech11 <soklenghoungofficial11@gmail.com>
2024-04-08 10:51:21 +0700

    close #1318 fix line-item searcher query

--- spec/queries/spree_cm_commissioner/line_item_seacher_query_spec.rb
+++ spec/queries/spree_cm_commissioner/line_item_seacher_query_spec.rb
@@ -11,13 +11,17 @@ RSpec.describe SpreeCmCommissioner::LineItemSearcherQuery do
     let!(:product_a) { create(:product, product_type: :ecommerce, taxons: [section_a]) }
     let!(:product_b) { create(:product, product_type: :ecommerce, taxons: [section_b]) }
 
-    let!(:order) { create(:order, phone_number: '012345678', email: 'lengvevo007@gmail.com', number: 'R591727627', completed_at: Date.current) }
+    let!(:order) { create(:order, phone_number: '012345678', number: 'R591727627', completed_at: Date.current) }
 
     let!(:line_item1) { create(:cm_line_item, order: order, product: product_a) }
     let!(:line_item2) { create(:cm_line_item, order: order, product: product_b) }
 
     let!(:guest1) { create(:guest, first_name: 'Han', last_name: 'Xin', line_item: line_item1) }
     let!(:guest2) { create(:guest, first_name: 'Sokleng', last_name: 'Houng', line_item: line_item2) }
+
+    before do
+      order.update_columns(email: 'lengvevo007@gmail.com')
+    end
 
     context 'when order qr_data is present' do
       let(:params) { { qr_data: 'R591727627-WiWma0OjZqh7Tk1aXGCUzA1708344610539' } }
@@ -35,7 +39,7 @@ RSpec.describe SpreeCmCommissioner::LineItemSearcherQuery do
       end
 
       it 'returns line items of the order found by qr_data' do
-        expect(described_class.new(params).call).to eq([line_item])
+        expect(described_class.new(params).call.to_a).to eq([line_item])
 
         expect(order).to have_received(:line_items)
       end
@@ -59,35 +63,32 @@ RSpec.describe SpreeCmCommissioner::LineItemSearcherQuery do
       end
 
       it 'returns line items found by term' do
-        expect(described_class.new(params).call).to eq([line_item2])
+        expect(described_class.new(params).call.to_a).to eq([line_item1, line_item2])
       end
 
       it 'returns line items found by term guest first_name' do
         params[:term] = 'Han'
-        expect(described_class.new(params).call).to eq([line_item1])
+        expect(described_class.new(params).call.to_a).to eq([line_item1])
       end
 
       it 'returns line items found by term guest last_name' do
-        params[:term] = 'Leng'
-        expect(described_class.new(params).call).to eq([line_item2])
+        params[:term] = 'Xin'
+        expect(described_class.new(params).call.to_a).to eq([line_item1])
       end
 
       it 'returns line items found by term order number' do
         params[:term] = 'R591727627'
-        expect(described_class.new(params).call).to eq([line_item1, line_item2])
+        expect(described_class.new(params).call.to_a).to eq([line_item1, line_item2])
       end
 
       it 'returns line items found by term order phone number' do
         params[:term] = '012345678'
-        expect(described_class.new(params).call).to eq([line_item1, line_item2])
+        expect(described_class.new(params).call.to_a).to eq([line_item1, line_item2])
       end
-    end
 
-    context 'when term email is present' do
-      let(:params) { { email: 'lengvevo007@gmail.com' } }
-
-      it 'returns line items found by email' do
-        expect(described_class.new(params).call).to eq([line_item1, line_item2])
+      it 'returns line items found by term email' do
+        params[:term] = 'lengvevo007@gmail.com'
+        expect(described_class.new(params).call.to_a).to eq([line_item1, line_item2])
       end
     end
 
