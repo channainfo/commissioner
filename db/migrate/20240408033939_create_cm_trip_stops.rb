62789c11a65ac9dc76b23f0850c88ddf269eb104
Tykea Ly <87005466+tykealy@users.noreply.github.com>
2024-04-26 10:50:11 +0700

    close#1346 search trip from stop (#1349)
    * WIP #1333 boarding and drop-off stop
    
    * fixed baording and drop-ff duplication record
    
    * prevent the same stop from being seleced twice
    
    * close #1346 modiefied trip search query to search from trip_stops

---
+++ db/migrate/20240408033939_create_cm_trip_stops.rb
@@ -0,0 +1,11 @@
+class CreateCmTripStops < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cm_trip_stops do |t|
+      t.integer :trip_id
+      t.integer :stop_id
+      t.integer :stop_type
+      t.string :stop_name
+      t.timestamps
+    end
+  end
+end
