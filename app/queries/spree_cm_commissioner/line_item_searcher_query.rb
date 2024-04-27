1d4dd1341e76ad9d831221cc7180934ee79085fc
Leng The Tech <soklenghoungofficial11@gmail.com>
2024-04-27 10:49:16 +0700

    close #1353 add search guest qr-data (#1354)

--- app/queries/spree_cm_commissioner/line_item_searcher_query.rb
+++ app/queries/spree_cm_commissioner/line_item_searcher_query.rb
@@ -6,8 +6,45 @@ module SpreeCmCommissioner
       @params = params
     end
 
+    def call
+      if @params[:qr_data].present?
+        if order_qr_data?
+          order = Spree::Order.search_by_qr_data!(@params[:qr_data])
+          order.line_items
+        elsif line_item_qr_data?
+          line_item_id = Spree::LineItem.search_by_qr_data!(@params[:qr_data]).id
+          Spree::LineItem.where(id: line_item_id)
+        else
+          guest = SpreeCmCommissioner::Guest.find_by!(token: @params[:qr_data])
+          Spree::LineItem.where(id: guest.line_item_id)
+        end
+      elsif @params[:term].present?
+        search_by_ransack
+      else
+        search_by_guest_infos
+      end
+    end
+
+    private
+
+    def order_qr_data?
+      matches = construct_matches
+      matches&.size == 2
+    end
+
+    def line_item_qr_data?
+      matches = construct_matches
+      matches&.size == 3
+    end
+
+    def guest_qr_data?
+      matches = construct_matches
+      matches.nil?
+    end
+
     def construct_matches
       qr_data = @params[:qr_data]
+      return nil unless qr_data
 
       if qr_data =~ /-L\d+$/
         qr_data.match(/(R\d+)-([A-Za-z0-9_\-]+)-(L\d+)/)&.captures
@@ -16,31 +53,9 @@ module SpreeCmCommissioner
       end
     end
 
-    def call
-      qr_data = @params[:qr_data]
-
-      if qr_data.present?
-        matches = construct_matches
-
-        if matches
-          if matches.size == 3
-            line_item_id = Spree::LineItem.search_by_qr_data!(qr_data).id
-            Spree::LineItem.where(id: line_item_id)
-          else
-            order = Spree::Order.search_by_qr_data!(qr_data)
-            order.line_items
-          end
-        end
-      elsif params[:term].present?
-        search_by_ransack
-      else
-        search_by_guest_infos
-      end
-    end
-
     def search_by_ransack
-      terms = params[:term].split
-      event_id = params[:event_id]
+      terms = @params[:term].split
+      event_id = @params[:event_id]
 
       Spree::LineItem
         .complete
@@ -51,10 +66,10 @@ module SpreeCmCommissioner
     end
 
     def search_by_guest_infos
-      first_name = params[:first_name]
-      last_name = params[:last_name]
-      phone_number = params[:phone_number]
-      taxon_id = params[:taxon_id]
+      first_name = @params[:first_name]
+      last_name = @params[:last_name]
+      phone_number = @params[:phone_number]
+      taxon_id = @params[:taxon_id]
 
       guests = SpreeCmCommissioner::Guest
                .joins(line_item: { variant: { product: { classifications: { taxon: { parent: :parent } } } } })
