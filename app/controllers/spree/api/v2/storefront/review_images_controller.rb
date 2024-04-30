4a5cb48f0d37480af29311f436506862ad096a0d
Chom Panha <92453740+panhachom@users.noreply.github.com>
2024-04-30 16:47:12 +0700

    close #1366 review images api (#1367)

---
+++ app/controllers/spree/api/v2/storefront/review_images_controller.rb
@@ -0,0 +1,31 @@
+module Spree
+  module Api
+    module V2
+      module Storefront
+        class ReviewImagesController < Spree::Api::V2::ResourceController
+          def create
+            context = SpreeCmCommissioner::ReviewImageCreator.call(
+              review: review,
+              url: params[:url]
+            )
+            if context.success?
+              render_serialized_payload { serialize_resource(context.result) }
+            else
+              render_error_payload(context.message)
+            end
+          end
+
+          private
+
+          def review
+            @review ||= Spree::Review.find(params[:review_id])
+          end
+
+          def resource_serializer
+            Spree::V2::Storefront::ReviewImageSerializer
+          end
+        end
+      end
+    end
+  end
+end
---
+++ app/models/spree_cm_commissioner/review_image.rb
@@ -0,0 +1,12 @@
+module SpreeCmCommissioner
+  class ReviewImage < Asset
+    protected
+
+    def asset_styles
+      {
+        thumb: '60x60>',
+        small: '180x180>'
+      }
+    end
+  end
+end
