728f595f5ecadc11f63b4a6ef744e9d8c4205ea7
Regene27 <l.regene27@gmail.com>
2024-05-02 16:09:14 +0700

    Close #1371 One invoice can has many subscriptions

--- spec/interactors/spree_cm_commissioner/subscribed_order_creator_spec.rb
+++ spec/interactors/spree_cm_commissioner/subscribed_order_creator_spec.rb
@@ -7,20 +7,41 @@ RSpec.describe SpreeCmCommissioner::SubscribedOrderCreator do
   let(:option_type) {create(:option_type, name: "due-date", attr_type: :integer)}
   let(:option_value){create(:option_value, name: "5 Days", presentation: "5", option_type: option_type)}
 
-  let(:product) { create(:base_product, option_types: [option_type], subscribable: true, vendor: vendor)}
-  let(:variant) { create(:base_variant, option_values: [option_value], price: 30, product: product, permanent_stock: 1 )}
+  let(:product1) { create(:base_product, option_types: [option_type], subscribable: true, vendor: vendor)}
+  let(:product2) { create(:base_product, option_types: [option_type], subscribable: true, vendor: vendor)}
+  let(:variant1) { create(:base_variant, option_values: [option_value], price: 30, product: product1, permanent_stock: 1 )}
+  let(:variant2) { create(:base_variant, option_values: [option_value], price: 30, product: product2, permanent_stock: 1 )}
+
+  let(:stock_location) { create(:stock_location, vendor: vendor) }
+  let(:stock_item1) { create(:stock_item, stock_location: stock_location, variant: variant1, count_on_hand: 10) }
+  let(:stock_item2) { create(:stock_item, stock_location: stock_location, variant: variant2, count_on_hand: 10) }
 
   describe ".call" do
-    it "return a newly created order" do
-      subscription = create(:cm_subscription, start_date: '2023-01-02'.to_date, customer: customer, price: 13.0, month: 1)
-      context = described_class.call(subscription: subscription)
-      expect(subscription.orders.size).to eq 2
-      expect(context.order.subscription_id).to eq subscription.id
-      expect(context.order.line_items[0].from_date).to eq subscription.start_date + 1.months
-      expect(context.order.line_items[0].to_date).to eq subscription.start_date + 2.months
-      expect(context.order.total).to eq subscription.variant.price
-      expect(context.order.user_id).to eq customer.user_id
-      expect(context.order.line_items[0].due_date).to eq context.order.line_items[0].from_date + 5.days
+    context "when customer has no prior subscription" do
+      it "return a newly created order" do
+        subscription = create(:cm_subscription, customer: customer, quantity: 2, start_date: '2023-01-02'.to_date)
+
+        context = described_class.call(subscription: subscription)
+
+        expect(context.order).to be_a(Spree::Order)
+        expect(context.order.customer).to eq customer
+        expect(context.order.line_items.size).to eq 1
+        expect(context.order.line_items[0].variant).to eq subscription.variant
+        expect(context.order.line_items[0].quantity).to eq 2
+      end
+    end
+
+    context "when customer has prior subscription" do
+      let(:subscription1) { create(:cm_subscription, customer: customer, quantity: 2, start_date: '2023-01-02'.to_date) }
+      let(:order1) { described_class.call(subscription: subscription1).order}
+
+      it "return the first order when customer has prior subscription" do
+        subscription2 = create(:cm_subscription, customer: customer, quantity: 2, start_date: '2023-01-02'.to_date)
+
+        context = described_class.call(subscription: subscription2)
+
+        expect(context.order).to eq(order1)
+      end
     end
 
     it 'created a default payment' do
@@ -28,31 +49,39 @@ RSpec.describe SpreeCmCommissioner::SubscribedOrderCreator do
 
       context = described_class.call(subscription: subscription)
 
-      expect(context.order.payments.size).to eq 1
-      expect(context.order.payments[0].amount).to eq context.order.order_total_after_store_credit
-      expect(context.order.payments[0].state).to eq 'checkout'
+      expect(context.order.payments.size).to eq 2
+      expect(context.order.payments[-1].amount).to eq context.order.order_total_after_store_credit
+      expect(context.order.payments[-1].state).to eq 'checkout'
     end
 
-    it 'create an invoice everytime a new order is created' do
+    it 'update invoice everytime a new service is subscribed' do
+      subscription1 = create(:cm_subscription, customer: customer, start_date: '2023-01-02'.to_date)
+      context1 = described_class.call(subscription: subscription1)
+
+      expect(context1.order.line_items.size).to eq 1
+
+      subscription2 = create(:cm_subscription, customer: customer)
+      context2 = described_class.call(subscription: subscription2)
+
+      expect(context1.order.line_items.size).to eq 2
+      expect(context1.order).to eq context2.order
+      expect(context1.order.invoice).to eq context2.order.invoice
+    end
+
+    it 'create an order with the correct due date with pre-paid option' do
       subscription = create(:cm_subscription, customer: customer, start_date: '2023-01-02'.to_date, price: 13.0, month: 1)
-      expect(subscription.orders.size).to eq 1
-      expect(subscription.orders[0].invoice.date).to eq subscription.start_date
 
       context = described_class.call(subscription: subscription)
-      expect(subscription.orders.size).to eq 2
-      expect(subscription.last_occurence).to eq subscription.start_date + 1.months
-      expect(subscription.orders[1].invoice.date).to eq subscription.last_occurence
+
+      expect(context.order.line_items.last.due_date).to eq '2023-01-07'.to_date
     end
 
-    it 'create an order with the correct due date' do
-      subscription1 = create(:cm_subscription, customer: customer, start_date: '2023-01-02'.to_date, price: 13.0, month: 1)
-      subscription2 = create(:cm_subscription, customer: customer, start_date: '2023-01-02'.to_date, price: 13.0, month: 1, payment_option:'post-paid')
-      expect(subscription1.orders.last.line_items.last.due_date).to eq subscription1.orders.last.line_items.last.from_date + 5.days
-      expect(subscription2.orders.last.line_items.last.due_date.to_date).to eq Date.parse('2023-02-07')
+    it 'create an order with the correct due date with post-paid option' do
+      subscription = create(:cm_subscription, customer: customer, start_date: '2023-01-02'.to_date, price: 13.0, month: 1, payment_option:'post-paid')
 
-      context = described_class.call(subscription: subscription2)
-      expect(subscription2.orders.size).to eq 2
-      expect(subscription2.orders.last.line_items.last.due_date.to_date).to eq Date.parse('2023-03-07')
+      context = described_class.call(subscription: subscription)
+
+      expect(context.order.line_items.last.due_date).to eq '2023-02-07'.to_date
     end
   end
 end
--- spec/queries/spree_cm_commissioner/subscription_revenue_overview_query_spec.rb
+++ spec/queries/spree_cm_commissioner/subscription_revenue_overview_query_spec.rb
@@ -1,20 +1,24 @@
 require 'spec_helper'
 
 RSpec.describe SpreeCmCommissioner::SubscriptionRevenueOverviewQuery do
-  let(:customer) { create(:cm_customer) }
+  let(:vendor) { create(:vendor) }
+  let(:customer1) { create(:cm_customer, vendor: vendor, sequence_number: 01) }
+  let(:customer2) { create(:cm_customer, vendor: vendor, sequence_number: 02) }
+  let(:customer3) { create(:cm_customer, vendor: vendor, sequence_number: 03) }
+
 
   describe '#reports' do
-    let(:subscription_jan2) { create(:cm_subscription, start_date: '2023-01-02'.to_date, customer: customer, price: 13.0, due_date: 5) }
-    let(:subscription_jan4) { create(:cm_subscription, start_date: '2023-01-04'.to_date, customer: customer, price: 25.0, due_date: 5) }
-    let(:subscription_feb4) { create(:cm_subscription, start_date: '2023-02-04'.to_date, customer: customer, price: 32.0, due_date: 5) }
+    let(:subscription_jan2) { create(:cm_subscription, start_date: '2023-01-02'.to_date, customer: customer1, price: 13.0, due_date: 5) }
+    let(:subscription_jan4) { create(:cm_subscription, start_date: '2023-01-04'.to_date, customer: customer2, price: 25.0, due_date: 5) }
+    let(:subscription_feb4) { create(:cm_subscription, start_date: '2023-02-04'.to_date, customer: customer3, price: 32.0, due_date: 5) }
 
     it 'only return totals in January' do
-      subscription_jan2.orders.each {|o| o.payments.each{|p| p.capture! }}
-      subscription_jan4.orders.each {|o| o.payments.each{|p| p.capture! }}
-      subscription_feb4.orders.each {|o| o.payments.each{|p| p.capture! }}
+      subscription_jan2.orders.each {|o| o.payments.last.capture! }
+      subscription_jan4.orders.each {|o| o.payments.last.capture! }
+      subscription_feb4.orders.each {|o| o.payments.last.capture! }
 
       # only for january
-      query = described_class.new(from_date: '2023-01-01', to_date: '2023-01-31', vendor_id: customer.vendor.id)
+      query = described_class.new(from_date: '2023-01-01', to_date: '2023-01-31', vendor_id: vendor.id)
 
       expect(query.reports).to match_array [
         {:orders_count => 2, :state => "paid", :total => 13.0 + 25.0, :payment_total => 13.0 + 25.0}
@@ -22,11 +26,11 @@ RSpec.describe SpreeCmCommissioner::SubscriptionRevenueOverviewQuery do
     end
 
     it 'return totals group by payment state' do
-      subscription_jan2.orders.each {|o| o.payments.each{|p| p.pend! }}
-      subscription_jan4.orders.each {|o| o.payments.each{|p| p.void! }}
-      subscription_feb4.orders.each {|o| o.payments.each{|p| p.capture! }}
+      subscription_jan2.orders.each {|o| o.payments.last.pend! }
+      subscription_jan4.orders.each {|o| o.payments.last.void! }
+      subscription_feb4.orders.each {|o| o.payments.last.capture! }
 
-      query = described_class.new(from_date: '2000-01-01', to_date: '2100-01-01', vendor_id: customer.vendor.id)
+      query = described_class.new(from_date: '2000-01-01', to_date: '2100-01-01', vendor_id: vendor.id)
 
       expect(query.reports).to match_array [
         {:orders_count => 1, :state => "paid", :total => 32.0, :payment_total => 32.0},
@@ -39,12 +43,12 @@ RSpec.describe SpreeCmCommissioner::SubscriptionRevenueOverviewQuery do
   # reference: subscription_orders_query_spec.rb
   describe '#reports_with_overdues' do
     it 'return reports + overdue on feb' do
-      subscription = create(:cm_subscription, start_date: '2023-01-02'.to_date, customer: customer, price: 13.0, month: 1, due_date: 5)
+      subscription = create(:cm_subscription, start_date: '2023-01-02'.to_date, customer: customer1, price: 13.0, month: 1, due_date: 5)
       subscription.orders[0].payments.each{|p| p.pend!}
 
       query = described_class.new(
         current_date: '2023-01-28',
-        vendor_id: customer.vendor.id,
+        vendor_id: vendor.id,
         from_date: '2000-01-01',
         to_date: '2100-01-01',
       )
