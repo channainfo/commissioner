05f0e06496fb5d3f00f207d35f31378f4396cb4d
Chom Panha <92453740+panhachom@users.noreply.github.com>
2024-05-07 18:01:41 +0700

    close #1402 rename option-values fields (#1403)

--- app/models/concerns/spree_cm_commissioner/variant_option_values_concern.rb
+++ app/models/concerns/spree_cm_commissioner/variant_option_values_concern.rb
@@ -4,15 +4,15 @@ module SpreeCmCommissioner
 
     # TODO: extract value from this.
     def started_at_option_value
-      option_value('started-at')
+      @started_at_option_value ||= option_value('started-at')
     end
 
     def reminder_option_value
-      option_value('reminder')
+      @reminder_option_value ||= option_value('reminder')
     end
 
     def delivery_option_option_value
-      option_value('delivery-option')
+      @delivery_option_option_value ||= option_value('delivery-option')
     end
 
     def max_quantity_per_order_option_value
@@ -23,6 +23,18 @@ module SpreeCmCommissioner
     # if null, users can add unlimited quantity as long as items are in stock.
     def max_quantity_per_order
       max_quantity_per_order_option_value&.to_i
+    end
+
+    def started_at
+      started_at_option_value
+    end
+
+    def reminder
+      reminder_option_value
+    end
+
+    def delivery_option
+      delivery_option_option_value
     end
   end
 end
