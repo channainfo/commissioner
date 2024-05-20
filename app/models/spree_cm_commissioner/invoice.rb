b79ea2f5f9ca463b261b2da56480db4aade27a9a
L.Regene <125754132+Regene27@users.noreply.github.com>
2024-05-20 09:16:24 +0700

    Close #1427 Filter invoice by invoice printed or not yet (#1428)

--- app/models/spree_cm_commissioner/invoice.rb
+++ app/models/spree_cm_commissioner/invoice.rb
@@ -5,5 +5,9 @@ module SpreeCmCommissioner
     belongs_to :order, class_name: 'Spree::Order', dependent: :destroy
     belongs_to :vendor, class_name: 'Spree::Vendor', dependent: :destroy
     validates :invoice_number, presence: true, uniqueness: { scope: :vendor_id }
+
+    def self.ransackable_attributes(auth_object = nil)
+      super & %w[invoice_issued_date]
+    end
   end
 end
