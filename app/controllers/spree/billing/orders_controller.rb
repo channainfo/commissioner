f3b29716f565d011ceb78ea9f92f60811094ef36
L.Regene <125754132+Regene27@users.noreply.github.com>
2024-06-04 17:47:48 +0700

    Close #1484 Monthly filtering invoices (#1493)

--- app/controllers/spree/billing/orders_controller.rb
+++ app/controllers/spree/billing/orders_controller.rb
@@ -20,6 +20,7 @@ module Spree
 
         load_customer
         load_subscription
+        filter_by_month(*default_date_range)
 
         @search = scope.ransack(params[:q])
         @collection = @search.result.includes(:subscription, :customer, :invoice).page(page).per(per_page)
@@ -37,6 +38,24 @@ module Spree
 
       def object_name
         'order'
+      end
+
+      def default_date_range
+        default_from_date = 6.months.ago.beginning_of_month
+        default_to_date = Time.current.end_of_month
+        [default_from_date, default_to_date]
+      end
+
+      def filter_by_month(default_from_date, default_to_date)
+        params[:q] ||= {}
+        if params[:month].present?
+          date = Date.parse("#{params[:month]}-01")
+          params[:q][:created_at_gteq] = date.beginning_of_month
+          params[:q][:created_at_lteq] = date.end_of_month
+        else
+          params[:q][:created_at_gteq] = default_from_date
+          params[:q][:created_at_lteq] = default_to_date
+        end
       end
     end
   end
