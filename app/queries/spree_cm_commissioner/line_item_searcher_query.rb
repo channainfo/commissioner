978295312fcc45d4401546cde066e4166f8f341d
Thea Choem <29684683+theachoem@users.noreply.github.com>
2024-07-04 15:34:46 +0700

    close #1587 refactor to support offline check-in

--- app/queries/spree_cm_commissioner/line_item_searcher_query.rb
+++ app/queries/spree_cm_commissioner/line_item_searcher_query.rb
@@ -6,22 +6,21 @@ module SpreeCmCommissioner
       @params = params
     end
 
+    def event_id = params[:event_id]
+
     def call
-      if @params[:qr_data].present?
-        if order_qr_data?
-          order = Spree::Order.search_by_qr_data!(@params[:qr_data])
-          order.line_items.complete
-        elsif line_item_qr_data?
-          line_item_id = Spree::LineItem.search_by_qr_data!(@params[:qr_data]).id
-          Spree::LineItem.complete.where(id: line_item_id)
-        else
-          guest = SpreeCmCommissioner::Guest.find_by!(token: @params[:qr_data])
-          Spree::LineItem.complete.where(id: guest.line_item_id)
-        end
-      elsif @params[:term].present?
+      return Spree::LineItem.none if params[:event_id].blank?
+
+      if params[:qr_data].present? && order_qr_data?
+        search_by_order_qr
+      elsif params[:qr_data].present? && line_item_qr_data?
+        search_by_line_item_qr
+      elsif params[:qr_data].present?
+        search_by_guest_qr
+      elsif params[:term].present?
         search_by_ransack
       else
-        search_by_guest_infos
+        all_line_items
       end
     end
 
@@ -43,7 +42,7 @@ module SpreeCmCommissioner
     end
 
     def construct_matches
-      qr_data = @params[:qr_data]
+      qr_data = params[:qr_data]
       return nil unless qr_data
 
       if qr_data =~ /-L\d+$/
@@ -53,41 +52,54 @@ module SpreeCmCommissioner
       end
     end
 
+    def search_by_order_qr
+      order = Spree::Order.complete.search_by_qr_data!(params[:qr_data])
+      order.line_items
+           .joins(:guests)
+           .where(guests: { event_id: event_id })
+           .distinct
+    end
+
+    def search_by_line_item_qr
+      line_item_id = Spree::LineItem.complete.search_by_qr_data!(params[:qr_data])&.id
+
+      Spree::LineItem
+        .joins(:guests)
+        .where(id: line_item_id, guests: { event_id: event_id })
+        .distinct
+    end
+
+    def search_by_guest_qr
+      guest = SpreeCmCommissioner::Guest.complete.find_by!(token: params[:qr_data], event_id: event_id)
+
+      Spree::LineItem.where(id: guest&.line_item_id)
+    end
+
     def search_by_ransack
-      terms = @params[:term].split
-      event_id = @params[:event_id]
+      terms = params[:term].split.map { |term| "%#{term.downcase}%" }
 
       Spree::LineItem
         .complete
-        .joins('INNER JOIN cm_guests ON cm_guests.line_item_id = spree_line_items.id')
-        .where(cm_guests: { event_id: event_id })
-        .ransack(guests_first_name_or_guests_last_name_or_order_phone_number_or_order_email_or_order_number_cont_any: terms)
-        .result
+        .joins(:guests)
+        .where(guests: { event_id: event_id })
+        .where(
+          "LOWER(spree_orders.number) LIKE ANY (ARRAY[:terms]) OR
+          LOWER(spree_line_items.number) LIKE ANY (ARRAY[:terms]) OR
+          LOWER(spree_orders.phone_number) LIKE ANY (ARRAY[:terms]) OR
+          LOWER(spree_orders.email) LIKE ANY (ARRAY[:terms]) OR
+          LOWER(guests.first_name) LIKE ANY (ARRAY[:terms]) OR
+          LOWER(guests.last_name) LIKE ANY (ARRAY[:terms])",
+          { terms: terms }
+        )
+        .distinct
     end
 
-    def search_by_guest_infos
-      first_name = @params[:first_name]
-      last_name = @params[:last_name]
-      phone_number = @params[:phone_number]
-      taxon_id = @params[:taxon_id]
-
-      guests = SpreeCmCommissioner::Guest.complete
-                                         .joins(line_item: { variant: { product: { classifications: { taxon: { parent: :parent } } } } })
-                                         .joins(line_item: :order)
-
-      if first_name.present? && last_name.present?
-        guests = guests.where('LOWER(cm_guests.first_name) LIKE LOWER(?) AND LOWER(cm_guests.last_name) LIKE LOWER(?)',
-                              "%#{first_name}%",
-                              "%#{last_name}%"
-                             )
-      end
-
-      guests = guests.where(spree_orders: { phone_number: phone_number }) if phone_number.present?
-
-      guests = guests.where(parent: { id: taxon_id }) if taxon_id.present?
-
-      line_item_ids = guests.map(&:line_item_id)
-      Spree::LineItem.where(id: line_item_ids)
+    def all_line_items
+      Spree::LineItem
+        .complete
+        .joins(:guests)
+        .where(guests: { event_id: event_id })
+        .distinct
     end
   end
 end
