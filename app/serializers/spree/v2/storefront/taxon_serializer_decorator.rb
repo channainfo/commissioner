afc66ec4b09843251c3b27679a065017445112ac
panhachom <panhachom@gmail.com>
2024-08-23 18:25:37 +0700

    Close #1821 Add preferred_background_color and preferred_foreground_color

--- app/serializers/spree/v2/storefront/taxon_serializer_decorator.rb
+++ app/serializers/spree/v2/storefront/taxon_serializer_decorator.rb
@@ -10,7 +10,7 @@ module Spree
           base.has_one :web_banner, serializer: ::SpreeCmCommissioner::V2::Storefront::AssetSerializer
           base.has_one :home_banner, serializer: ::SpreeCmCommissioner::V2::Storefront::AssetSerializer
 
-          base.attributes :custom_redirect_url, :kind, :subtitle, :from_date, :to_date
+          base.attributes :custom_redirect_url, :kind, :subtitle, :from_date, :to_date, :background_color, :foreground_color
         end
       end
     end
