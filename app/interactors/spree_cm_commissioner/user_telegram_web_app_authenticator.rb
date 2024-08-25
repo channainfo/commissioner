2623775da264593305aa9abee21e8c7d0b356359
Thea Choem <29684683+theachoem@users.noreply.github.com>
2024-08-25 14:52:38 +0700

    close #1829 fix telegram incorrect translation & remove unnessary struct

--- app/interactors/spree_cm_commissioner/user_telegram_web_app_authenticator.rb
+++ app/interactors/spree_cm_commissioner/user_telegram_web_app_authenticator.rb
@@ -4,9 +4,9 @@ module SpreeCmCommissioner
 
     def call
       return context.fail!(message: 'telegram_bot_username_is_not_valid_or_not_exist_in_db') if telegram_bot.blank?
-      return context.fail!(message: 'invalid_init_data') if checker.blank?
+      return context.fail!(message: 'invalid_init_data') unless checker_context.success?
 
-      context.telegram_user = ActiveSupport::JSON.decode(checker.checker_context.decoded_telegram_init_data['user'])
+      context.telegram_user = ActiveSupport::JSON.decode(checker_context.decoded_telegram_init_data['user'])
       context.user = find_or_create_telegram_user
     end
 
@@ -33,17 +33,12 @@ module SpreeCmCommissioner
       end
     end
 
-    def checker
-      result = TelegramWebAppInitDataValidator.call(telegram_init_data: telegram_init_data, bot_token: telegram_bot.token)
-      context.checker ||= checker_struct.new(result, telegram_bot) if result.success?
+    def checker_context
+      context.checker_context ||= TelegramWebAppInitDataValidator.call(telegram_init_data: telegram_init_data, bot_token: telegram_bot.token)
     end
 
     def telegram_bot
       context.telegram_bot ||= TelegramBot.find_by(username: telegram_bot_username)
-    end
-
-    def checker_struct
-      Struct.new(:checker_context, :telegram_bot)
     end
   end
 end
