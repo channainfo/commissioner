284895971ca7064e5893ab24fe1c4d4b382c92c1
Thea Choem <29684683+theachoem@users.noreply.github.com>
2024-10-09 17:38:52 +0700

    close #2002 fix resource not found when create new guest (#2003)

--- app/controllers/spree/api/v2/storefront/guests_controller.rb
+++ app/controllers/spree/api/v2/storefront/guests_controller.rb
@@ -3,14 +3,14 @@ module Spree
     module V2
       module Storefront
         class GuestsController < ::Spree::Api::V2::ResourceController
+          include OrderConcern
+
+          # raise 404 when no order
+          before_action :ensure_order
+
           # override
           def model_class
             SpreeCmCommissioner::Guest
-          end
-
-          # override
-          def collection
-            model_class.all
           end
 
           # override
@@ -24,7 +24,9 @@ module Spree
           end
 
           def create
-            resource = parent.guests.create(guest_params)
+            spree_authorize! :update, spree_current_order, order_token
+
+            resource = scope.new(guest_params)
 
             if resource.save
               render_serialized_payload(201) { serialize_resource(resource) }
@@ -34,9 +36,9 @@ module Spree
           end
 
           def update
-            resource.assign_attributes(guest_params)
+            spree_authorize! :update, spree_current_order, order_token
 
-            if resource.save
+            if resource.update(guest_params)
               render_serialized_payload { serialize_resource(resource) }
             else
               render_error_payload(resource, 400)
@@ -51,11 +53,7 @@ module Spree
           end
 
           def parent
-            @parent ||= if spree_current_user.nil?
-                          Spree::LineItem.find(params[:line_item_id])
-                        else
-                          spree_current_user.line_items.find(params[:line_item_id])
-                        end
+            @parent ||= spree_current_order.line_items.find(params[:line_item_id])
           end
 
           def guest_params
