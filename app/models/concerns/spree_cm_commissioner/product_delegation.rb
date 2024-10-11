486bd2666b562c67cba1c1f8445170a99d489a72
nangdypanhar <nangdypanhar@gmail.com>
2024-10-11 19:20:54 +0700

    close #1972 allow admin to enable or disable self check in

--- app/models/concerns/spree_cm_commissioner/product_delegation.rb
+++ app/models/concerns/spree_cm_commissioner/product_delegation.rb
@@ -11,6 +11,7 @@ module SpreeCmCommissioner
                :accommodation?, :service?, :ecommerce?,
                :associated_event,
                :allow_self_check_in,
+               :allow_self_check_in?,
                to: :product
     end
   end
