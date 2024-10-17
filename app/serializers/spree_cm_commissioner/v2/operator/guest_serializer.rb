e39ce1e92727ea6c2e47566fb443fe90f031dd85
panhachom <panhachom@gmail.com>
2024-10-17 14:50:29 +0700

    2018 add attributes to guest serializer operator

--- app/serializers/spree_cm_commissioner/v2/operator/guest_serializer.rb
+++ app/serializers/spree_cm_commissioner/v2/operator/guest_serializer.rb
@@ -4,8 +4,11 @@ module SpreeCmCommissioner
       class GuestSerializer < BaseSerializer
         set_type :guest
 
-        attributes :first_name, :last_name, :dob, :gender, :qr_data, :event_id, :seat_number, :formatted_bib_number, :phone_number
+        attributes :first_name, :last_name, :dob, :gender, :kyc_fields, :other_occupation, :created_at, :updated_at,
+                   :qr_data, :social_contact_platform, :social_contact, :available_social_contact_platforms, :event_id, :seat_number,
+                   :age, :emergency_contact, :other_organization, :expectation, :upload_later, :address, :formatted_bib_number, :phone_number
 
+        attribute :allowed_checkout, &:allowed_checkout?
         attribute :require_kyc_field, &:require_kyc_field?
 
         belongs_to :occupation, serializer: Spree::V2::Storefront::TaxonSerializer
