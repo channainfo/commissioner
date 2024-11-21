10a90787dd718a98fa978a5fd72f2e135d190ec1
Thea Choem <29684683+theachoem@users.noreply.github.com>
2024-11-21 17:39:15 +0700

    Revert "close #1968 implement CRUD template_guests with softdelete (#1981)"
    This reverts commit 8a2f51bd88e0495d5f263a7da2d884341c591284.

--- app/models/spree_cm_commissioner/id_card.rb
+++ app/models/spree_cm_commissioner/id_card.rb
@@ -1,11 +1,8 @@
 module SpreeCmCommissioner
   class IdCard < SpreeCmCommissioner::Base
-    # Polymorphic association
-    belongs_to :id_cardable, polymorphic: true
-
     enum card_type: { :national_id_card => 0, :passport => 1, :student_id_card => 2 }
 
-    # belongs_to :guest, class_name: 'SpreeCmCommissioner::Guest'
+    belongs_to :guest, class_name: 'SpreeCmCommissioner::Guest'
 
     has_one :front_image, as: :viewable, dependent: :destroy, class_name: 'SpreeCmCommissioner::FrontImage'
     has_one :back_image, as: :viewable, dependent: :destroy, class_name: 'SpreeCmCommissioner::BackImage'
