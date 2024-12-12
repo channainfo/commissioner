d59b17dbda35cc003e71443e8c1895931a2e1c46
Sokleng Houng <soklenghoungofficial11@gmail.com>
2024-12-12 16:30:08 +0700

    close #2133 enchance N+1 admin/products (#2142)

--- app/controllers/spree_cm_commissioner/admin/products_controller_decorator.rb
+++ app/controllers/spree_cm_commissioner/admin/products_controller_decorator.rb
@@ -5,6 +5,38 @@ module SpreeCmCommissioner
         # spree update user sign_in_count
         base.around_action :set_writing_role, only: %i[index]
       end
+
+      # Override
+      def collection
+        return @collection if @collection.present?
+
+        params[:q] ||= {}
+        params[:q][:deleted_at_null] ||= '1'
+        params[:q][:s] ||= 'name asc'
+
+        @collection = product_scope
+
+        @collection = @collection.with_deleted if params[:q][:deleted_at_null] == '0'
+
+        @search = @collection.ransack(params[:q].reject { |k, _v| k.to_s == 'deleted_at_null' })
+
+        @collection = @search.result
+                             .includes(*product_includes)
+                             .page(params[:page])
+                             .per(params[:per_page] || 10)
+
+        @collection
+      end
+
+      # Override
+      def product_includes
+        [
+          :vendor,
+          :shipping_category,
+          { master: :images },
+          :variants
+        ]
+      end
     end
   end
 end
