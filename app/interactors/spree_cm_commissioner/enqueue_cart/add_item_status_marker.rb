73b968eee0e8b6aaf1446110d516156201875f66
Chom Panha <92453740+panhachom@users.noreply.github.com>
2024-12-13 19:54:19 +0700

    close #2148 segment data by add item queue (#2149)

--- app/interactors/spree_cm_commissioner/enqueue_cart/add_item_status_marker.rb
+++ app/interactors/spree_cm_commissioner/enqueue_cart/add_item_status_marker.rb
@@ -15,22 +15,14 @@ module SpreeCmCommissioner
         assign_firestore_context_attributes
       end
 
-      def firestore
-        @firestore ||= Google::Cloud::Firestore.new(project_id: service_account[:project_id], credentials: service_account)
-      end
-
-      def service_account
-        @service_account ||= Rails.application.credentials.cloud_firestore_service_account
-      end
-
       def update_cart_firestore_status
         data_to_set = build_data_to_set
 
-        firestore.col('queues')
-                 .doc('cart')
-                 .col(order_number)
-                 .doc(job_id)
-                 .set(data_to_set, merge: true)
+        firestore_reference.set(data_to_set, merge: true)
+      end
+
+      def current_date
+        Time.current.strftime('%Y-%m-%d')
       end
 
       def build_data_to_set
@@ -42,33 +34,32 @@ module SpreeCmCommissioner
         }.compact
       end
 
-      def firestore_object
-        firestore.col('queues').doc('cart').col(order_number).doc(job_id).get
-      end
-
-      def firestore_status
-        firestore_object[:status]
-      end
-
-      def firestore_queued_at
-        firestore_object[:queued_at]
-      end
-
-      def firestore_collection_reference
-        firestore_object.ref.path
-      end
-
-      def firestore_document_reference
-        firestore_collection_reference.split('/documents').last
-      end
-
       def assign_firestore_context_attributes
-        document = firestore_object
+        document = firestore_reference.get
 
         context.firestore_status = document[:status]
         context.firestore_queued_at = document[:queued_at]
-        context.firestore_collection_reference = document.ref.path
         context.firestore_document_reference = document.ref.path.split('/documents').last
+        context.firestore_collection_reference = context.firestore_document_reference.rpartition('/').first
+      end
+
+      private
+
+      def firestore_reference
+        firestore.col('queues')
+                 .doc('cart')
+                 .col(current_date)
+                 .doc(order_number)
+                 .col('records')
+                 .doc(job_id)
+      end
+
+      def firestore
+        @firestore ||= Google::Cloud::Firestore.new(project_id: service_account[:project_id], credentials: service_account)
+      end
+
+      def service_account
+        @service_account ||= Rails.application.credentials.cloud_firestore_service_account
       end
     end
   end
