ed9d7a5b441afd007e05e3204e89f6ea6657a51e
Sambo <koysambo007@gmail.com>
2025-01-08 14:41:03 +0700

    Close #2226 organizer create ticket

--- app/controllers/spree/api/v2/organizer/tickets_controller.rb
+++ app/controllers/spree/api/v2/organizer/tickets_controller.rb
@@ -3,6 +3,9 @@ module Spree
     module V2
       module Organizer
         class TicketsController < ::Spree::Api::V2::Organizer::BaseController
+          before_action :load_store, only: %i[create]
+          before_action :load_event, only: %i[create]
+
           def index
             event = Spree::Taxon.find(params[:event_id])
 
@@ -24,8 +27,68 @@ module Spree
             end
           end
 
+          def create
+            resource = Spree::Product.new(product_params)
+            resource.stores << @store
+            resource.taxons << @event
+
+            if resource.save
+              render_serialized_payload(201) { serialize_resource(resource) }
+            else
+              render_error_payload(resource.errors.full_messages.to_sentence, 400)
+            end
+          end
+
+          def update
+            resource = Spree::Product.find(params[:id])
+
+            if resource.update(product_params)
+              render_serialized_payload { serialize_resource(resource) }
+            else
+              render_error_payload(resource.errors.full_messages.to_sentence, 400)
+            end
+          end
+
+          def destroy
+            resource = Spree::Product.find(params[:id])
+
+            if resource.destroy
+              render_serialized_payload { serialize_resource(resource) }
+            else
+              render_error_payload(resource.errors.full_messages.to_sentence, 400)
+            end
+          end
+
+          private
+
+          def load_store
+            @store ||= Spree::Store.first
+          end
+
+          def load_event
+            @event ||= Spree::Taxon.find_by(id: params[:event_id])
+          end
+
           def collection_serializer
             ::Spree::V2::Organizer::TicketSerializer
+          end
+
+          def resource_serializer
+            ::Spree::V2::Organizer::TicketSerializer
+          end
+
+          def product_params
+            params.permit(
+              :name,
+              :price,
+              :compare_at_price,
+              :available_on,
+              :kyc,
+              :description,
+              :shipping_category_id,
+              :product_type,
+              :status
+            )
           end
         end
       end
--- app/serializers/spree/v2/organizer/ticket_serializer.rb
+++ app/serializers/spree/v2/organizer/ticket_serializer.rb
@@ -2,7 +2,6 @@ module Spree
   module V2
     module Organizer
       class TicketSerializer < BaseSerializer
-        set_type :ticket
         attributes :name, :price, :compare_at_price, :available_on, :kyc, :description, :shipping_category_id, :product_type, :status
       end
     end
