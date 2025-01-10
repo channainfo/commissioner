df15c5f0f38c41f292ffa52f07c32898c21ea3eb
LengTech11 <soklenghoungofficial11@gmail.com>
2025-01-10 11:09:02 +0700

    close #2236 fix noticed response content cachable

--- app/controllers/concerns/spree_cm_commissioner/content_cachable.rb
+++ app/controllers/concerns/spree_cm_commissioner/content_cachable.rb
@@ -11,6 +11,9 @@ module SpreeCmCommissioner
     end
 
     def set_cache_control_for_cdn
+      return if response.committed?
+      return if response.status != 200
+
       # max-age: browser cache, s-maxage: server cache
       response.headers['Cache-Control'] = "public, max-age=0, s-maxage=#{max_age}"
       response.headers['Pragma'] = 'no-cache' # For older HTTP/1.0 clients
