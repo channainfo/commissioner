bbdd030d9ab112833b7ef8eee771d8ea8aef2858
Channa Ly <channa.info@gmail.com>
2025-01-10 14:40:00 +0700

    Close #2239 make block forwarding consistent (#2240)

--- app/controllers/spree_cm_commissioner/application_controller_decorator.rb
+++ app/controllers/spree_cm_commissioner/application_controller_decorator.rb
@@ -6,8 +6,8 @@ module SpreeCmCommissioner
     end
 
     # Annonymous block: https://www.rubydoc.info/gems/rubocop/RuboCop/Cop/Naming/BlockForwarding
-    def set_writing_role(&)
-      ActiveRecord::Base.connected_to(role: :writing, &)
+    def set_writing_role(&block)
+      ActiveRecord::Base.connected_to(role: :writing, &block)
     end
 
     def after_sign_in_path_for(_)
