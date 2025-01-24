7134bf9a1a91ce77eefd66fd73a7128c9023c28e
LengTech11 <soklenghoungofficial11@gmail.com>
2025-01-24 10:49:51 +0700

    close #2293 Create Api tenant vendor

---
+++ app/controllers/spree/api/v2/tenant/vendors_controller.rb
@@ -0,0 +1,40 @@
+module Spree
+  module Api
+    module V2
+      module Tenant
+        class VendorsController < ::Spree::Api::V2::Tenant::BaseController
+          def index
+            collection = scope.page(params[:page])
+                              .per(params[:per_page])
+
+            render_serialized_payload do
+              serialize_collection(collection)
+            end
+          end
+
+          def show
+            resource = scope.find(params[:id])
+
+            render_serialized_payload do
+              serialize_resource(resource)
+            end
+          end
+
+          def scope
+            MultiTenant.with(@tenant) do
+              ::Spree::Vendor.active
+            end
+          end
+
+          def resource_serializer
+            Spree::V2::Tenant::VendorSerializer
+          end
+
+          def collection_serializer
+            Spree::V2::Tenant::VendorSerializer
+          end
+        end
+      end
+    end
+  end
+end
