4374090d0d249d666251e6dec2d613db9cbb6650
nangdypanhar <181545922+nangdypanhar@users.noreply.github.com>
2025-01-30 15:35:07 +0700

    close #2291 remove homepage section when events are inactive

--- app/models/spree_cm_commissioner/homepage_section.rb
+++ app/models/spree_cm_commissioner/homepage_section.rb
@@ -8,7 +8,13 @@ module SpreeCmCommissioner
 
     has_many :homepage_section_relatables, -> { active }, inverse_of: :homepage_section, dependent: :destroy
 
-    scope :active, -> { where(active: true).order(:position) }
+    scope :active, lambda {
+      joins(:homepage_section_relatables)
+        .where(active: true)
+        .group('cm_homepage_sections.id')
+        .having('COUNT(cm_homepage_section_relatables.id) > 0')
+        .order(:position)
+    }
 
     scope :filter_by_segment, -> (segment) { where('segment & ? > 0', BIT_SEGMENT[segment.to_sym]) }
   end
