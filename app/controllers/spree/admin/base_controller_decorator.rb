c82a244d12ab4d429f7c55841ec838ecd0ba927d
sokly <sokly@soklys-MacBook-Pro.local>
2025-02-07 14:09:31 +0700

    Close #2334: Add Dockerfile, and fix spec

--- app/controllers/spree/admin/base_controller_decorator.rb
+++ app/controllers/spree/admin/base_controller_decorator.rb
@@ -1,6 +1,21 @@
 module Spree
   module Admin
     module BaseControllerDecorator
+      # even db in read only mode, authorizor still need to run Warden callbacks which will write to db
+      # this include update tracked fields such as sign_in_count, last_sign_in_at, etc.
+      def authorize!(*args)
+        ActiveRecord::Base.connected_to(role: :writing) do
+          super
+        end
+      end
+
+      # override: even in view actions, head.html.erb still need admin_oauth_token which may create new token.
+      def admin_oauth_token
+        ActiveRecord::Base.connected_to(role: :writing) do
+          super
+        end
+      end
+
       # 2023-07-31
       def parse_date!(date, format: nil)
         Date.strptime(date.to_s, format || '%Y-%m-%d')
