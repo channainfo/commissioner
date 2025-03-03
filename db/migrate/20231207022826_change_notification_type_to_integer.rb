a802d603b680de1f1400bc4aceb44a98f9f7610a
Channa Ly <channa.info@gmail.com>
2025-03-03 16:01:13 +0700

    Close #2405 build and publish gem

--- db/migrate/20231207022826_change_notification_type_to_integer.rb
+++ db/migrate/20231207022826_change_notification_type_to_integer.rb
@@ -1,4 +1,4 @@
-class ChangeNotificationTypeToIntegerInSpreeCmCommissionerCustomerNotifications < ActiveRecord::Migration[7.0]
+class ChangeNotificationTypeToInteger < ActiveRecord::Migration[7.0]
   def change
     change_column :cm_customer_notifications, :notification_type, :integer, using: 'notification_type::integer', default: 0
   end
