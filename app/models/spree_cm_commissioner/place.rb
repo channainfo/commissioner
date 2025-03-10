645d4722f9a8e114b146f79a207d4b07dacfd72e
Un Sreypich <unsreypich999@gmail.com>
2025-03-10 15:27:11 +0700

    Close #2428 Add parent-child relationship to Place model and migration for parent_id (#2429)

--- app/models/spree_cm_commissioner/place.rb
+++ app/models/spree_cm_commissioner/place.rb
@@ -13,6 +13,8 @@ module SpreeCmCommissioner
 
     has_many :product_places, class_name: 'SpreeCmCommissioner::ProductPlace', dependent: :destroy
     has_many :products, through: :product_places
+    has_many :children, class_name: 'SpreeCmCommissioner::Place', foreign_key: :parent_id, dependent: :destroy
+    belongs_to :parent, class_name: 'SpreeCmCommissioner::Place', optional: true
 
     def self.ransackable_attributes(auth_object = nil)
       super & %w[name code]
