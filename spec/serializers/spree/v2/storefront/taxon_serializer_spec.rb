5107cd97d17f6f16515d95308871730003432074
SreyMoch <orngsreymoch@gmail.com>
2025-03-27 15:22:53 +0700

    Close #2490 add check button for hide video banner

--- spec/serializers/spree/v2/storefront/taxon_serializer_spec.rb
+++ spec/serializers/spree/v2/storefront/taxon_serializer_spec.rb
@@ -52,7 +52,8 @@ RSpec.describe Spree::V2::Storefront::TaxonSerializer, type: :serializer do
         :purchasable_on_app,
         :purchasable_on_web,
         :vendor_id,
-        :available_on
+        :available_on,
+        :hide_video_banner
       )
     end
 
