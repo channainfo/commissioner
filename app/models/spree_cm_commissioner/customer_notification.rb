d2ef59b477ed00e7cf8d7d896e3870bcfe5fa9b8
LengTech11 <soklenghoungofficial11@gmail.com>
2025-04-24 17:01:00 +0700

    Close #2546 Org Dashboard Notifications Push

--- app/models/spree_cm_commissioner/customer_notification.rb
+++ app/models/spree_cm_commissioner/customer_notification.rb
@@ -6,13 +6,18 @@ module SpreeCmCommissioner
     has_one :feature_image, as: :viewable, dependent: :destroy, class_name: 'SpreeCmCommissioner::FeatureImage'
 
     has_many :feature_images, as: :viewable, dependent: :destroy, class_name: 'SpreeCmCommissioner::FeatureImage'
-    has_many :notification_taxons, class_name: 'SpreeCmCommissioner::NotificationTaxon'
+    has_many :notification_taxons, dependent: :destroy, class_name: 'SpreeCmCommissioner::NotificationTaxon'
     has_many :taxons, through: :notification_taxons, class_name: 'Spree::Taxon'
+
+    belongs_to :vendor, class_name: 'Spree::Vendor'
 
     validates :title, presence: true
     validates :url, presence: true
 
     validates :notification_type, presence: true
+
+    self.whitelisted_ransackable_attributes = %w[title excerpt notification_type]
+    self.whitelisted_ransackable_associations = %w[notification_taxons taxons]
 
     enum notification_type: { :promotion => 0, :announcement => 1 }
 
