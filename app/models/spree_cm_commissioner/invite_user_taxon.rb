095efce1f625eb23f5491241725adcee89c7f043
Chom Panha <92453740+panhachom@users.noreply.github.com>
2025-04-29 15:04:58 +0700

    close #2566 update crew invite email icon (#2567)

--- app/models/spree_cm_commissioner/invite_user_taxon.rb
+++ app/models/spree_cm_commissioner/invite_user_taxon.rb
@@ -2,6 +2,7 @@ module SpreeCmCommissioner
   class InviteUserTaxon < SpreeCmCommissioner::Base
     belongs_to :user_taxon, class_name: 'SpreeCmCommissioner::UserTaxon'
     belongs_to :invite, class_name: 'SpreeCmCommissioner::Invite'
+    has_one :inviter, class_name: 'Spree::User', through: :invite
     after_create :send_crew_invite_email
 
     def send_crew_invite_email
