1a171386afe77f064a74dbbdad1583fe3ea010b4
Sambo <koysambo007@gmail.com>
2025-04-29 17:20:17 +0700

    Close #2584 implement CreateEvent service

---
+++ app/interactors/spree_cm_commissioner/create_event.rb
@@ -0,0 +1,65 @@
+module SpreeCmCommissioner
+  class CreateEvent
+    include Interactor
+    delegate :params, :current_vendor, to: :context
+
+    def call
+      ActiveRecord::Base.transaction do
+        build_parent_taxon
+        assign_vendor
+        assign_prototype
+        create_child_taxon
+        build_home_banner
+      end
+    end
+
+    private
+
+    def build_parent_taxon
+      @parent_taxon = Spree::Taxon.new(
+        name: context.params[:name],
+        description: context.params[:description],
+        from_date: context.params[:from_date],
+        to_date: context.params[:to_date],
+        parent_id: context.params[:parent_id],
+        taxonomy_id: context.params[:taxonomy_id]
+      )
+
+      return if @parent_taxon.save
+
+      context.fail!(message: @parent_taxon.errors.full_messages.join(', '))
+    end
+
+    def assign_vendor
+      @parent_taxon.vendors << context.current_vendor
+    end
+
+    def assign_prototype
+      prototype = Spree::Prototype.find_by(slug: context.params[:prototype_id])
+      @parent_taxon.prototypes << prototype if prototype.present?
+    end
+
+    def create_child_taxon
+      child_taxon = Spree::Taxon.new(
+        name: 'Ticket Type',
+        parent_id: @parent_taxon.id,
+        taxonomy_id: context.params[:taxonomy_id]
+      )
+
+      return if child_taxon.save
+
+      context.fail!(message: child_taxon.errors.full_messages.join(', '))
+    end
+
+    def build_home_banner
+      return if context.params[:home_banner].nil?
+
+      banner = SpreeCmCommissioner::TaxonHomeBanner.create(
+        viewable: @parent_taxon,
+        attachment: context.params[:home_banner]
+      )
+
+      context.fail!(message: 'Home banner upload failed') unless banner.persisted?
+    end
+  end
+end
