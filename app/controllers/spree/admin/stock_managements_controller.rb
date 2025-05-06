101e89981666cf64fe5b0a00d82ec0ccf0ab934d
Thea Choem <29684683+theachoem@users.noreply.github.com>
2025-05-06 20:55:24 +0700

    Close #2578 display redis inventory to UI (#2615)
    * Close #2519 run redis in spec
    
    * Close #2606 fix not to deduct stock when not track inventory
    
    * Close #2578 display redis inventory in UI
    
    ---------
    
    Co-authored-by: hengsokly <hengsokly23@gmail.com>

--- app/controllers/spree/admin/stock_managements_controller.rb
+++ app/controllers/spree/admin/stock_managements_controller.rb
@@ -5,6 +5,8 @@ module Spree
 
       before_action :load_parent
 
+      helper_method :inventory_item_message
+
       def load_parent
         @product = Spree::Product.find_by(slug: params[:product_id])
       end
@@ -24,13 +26,29 @@ module Spree
         to_date = Date.new(@year, 1, 1).end_of_year
 
         @inventory_items = @product.inventory_items.includes(:variant).where(inventory_date: from_date..to_date).to_a
+        @cached_inventory_items = ::SpreeCmCommissioner::RedisStock::CachedInventoryItemsBuilder.new(@inventory_items)
+                                                                                                .call
+                                                                                                .index_by(&:inventory_item_id)
         @events = SpreeCmCommissioner::CalendarEvent.from_inventory_items(@inventory_items)
+      end
+
+      def inventory_item_message(inventory_item, cached_inventory_item)
+        synced = inventory_item.quantity_available == cached_inventory_item.quantity_available
+
+        if synced
+          "Synced: Quantity available matches in both DB and Redis (#{cached_inventory_item.quantity_available})."
+        else
+          "Out of sync: Redis shows #{cached_inventory_item.quantity_available} available, which doesn't match the database."
+        end
       end
 
       private
 
       def load_inventories
         @inventory_items = @product.inventory_items.group_by { |item| item.variant.id }
+        @cached_inventory_items = ::SpreeCmCommissioner::RedisStock::CachedInventoryItemsBuilder.new(@product.inventory_items)
+                                                                                                .call
+                                                                                                .index_by(&:inventory_item_id)
         @reserved_stocks = Spree::LineItem
                            .complete
                            .where(variant_id: @variants.pluck(:id))
--- app/interactors/spree_cm_commissioner/ensure_correct_product_type.rb
+++ app/interactors/spree_cm_commissioner/ensure_correct_product_type.rb
@@ -2,20 +2,39 @@ module SpreeCmCommissioner
   class EnsureCorrectProductType < BaseInteractor
     def call
       Spree::Product
-        .joins(variants_including_master: %i[inventory_items line_items])
+        .left_joins(variants_including_master: %i[inventory_items line_items])
         .where(
-          'spree_variants.product_type != spree_products.product_type OR
+          'spree_variants.product_type IS NULL OR
+          spree_variants.product_type != spree_products.product_type OR
+
+          cm_inventory_items.product_type IS NULL OR
           cm_inventory_items.product_type != spree_products.product_type OR
-          spree_line_items.product_type != spree_products.product_type'
-        ).distinct.find_each do |product|
+
+          spree_line_items.product_type IS NULL OR
+          spree_line_items.product_type != spree_products.product_type OR
+
+          spree_products.product_type IS NOT NULL
+          '
+        )
+        .distinct.find_each do |product|
         sync_product_type_for(product)
       end
     end
 
     def sync_product_type_for(product)
-      product.variants_including_master.where.not(product_type: product.product_type).update_all(product_type: product.product_type) # rubocop:disable Rails/SkipsModelValidations
-      product.line_items.where.not(product_type: product.product_type).update_all(product_type: product.product_type) # rubocop:disable Rails/SkipsModelValidations
-      product.inventory_items.where.not(product_type: product.product_type).update_all(product_type: product.product_type) # rubocop:disable Rails/SkipsModelValidations
+      product_type = Spree::Variant.product_types[product.product_type]
+
+      product.variants_including_master
+             .where('spree_variants.product_type IS NULL OR spree_variants.product_type != ?', product_type)
+             .update_all(product_type: product_type) # rubocop:disable Rails/SkipsModelValidations
+
+      product.line_items
+             .where('spree_line_items.product_type IS NULL OR spree_line_items.product_type != ?', product_type)
+             .update_all(product_type: product_type) # rubocop:disable Rails/SkipsModelValidations
+
+      product.inventory_items
+             .where('cm_inventory_items.product_type IS NULL OR cm_inventory_items.product_type != ?', product_type)
+             .update_all(product_type: product_type) # rubocop:disable Rails/SkipsModelValidations
     end
   end
 end
