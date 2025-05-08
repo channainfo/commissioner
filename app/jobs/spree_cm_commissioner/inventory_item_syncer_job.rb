606159f53699e2d407f7fbddf5bc9470b5f86f38
hengsokly <hengsokly23@gmail.com>
2025-05-08 17:07:22 +0700

    Close #2577 add log to critical logic code

--- app/jobs/spree_cm_commissioner/inventory_item_syncer_job.rb
+++ app/jobs/spree_cm_commissioner/inventory_item_syncer_job.rb
@@ -1,7 +1,11 @@
 module SpreeCmCommissioner
   class InventoryItemSyncerJob < ApplicationUniqueJob
-    def perform(inventory_id_and_quantities:)
-      InventoryItemSyncer.call(inventory_id_and_quantities:)
+    def perform(inventory_id_and_quantities:, line_item_ids:)
+      CmAppLogger.log(label: 'InventoryItemSyncerJob',
+                      data: { inventory_id_and_quantities: inventory_id_and_quantities, line_item_ids: line_item_ids }
+                     ) do
+        InventoryItemSyncer.call(inventory_id_and_quantities: inventory_id_and_quantities)
+      end
     end
   end
 end
--- app/jobs/spree_cm_commissioner/stock/inventory_items_adjuster_job.rb
+++ app/jobs/spree_cm_commissioner/stock/inventory_items_adjuster_job.rb
@@ -4,7 +4,9 @@ module SpreeCmCommissioner
       def perform(variant_id:, quantity:)
         variant = Spree::Variant.find(variant_id)
 
-        SpreeCmCommissioner::Stock::InventoryItemsAdjuster.call(variant: variant, quantity: quantity)
+        CmAppLogger.log(label: 'InventoryItemsAdjusterJob', data: { variant_id: variant_id, quantity: quantity }) do
+          SpreeCmCommissioner::Stock::InventoryItemsAdjuster.call(variant: variant, quantity: quantity)
+        end
       end
     end
   end
