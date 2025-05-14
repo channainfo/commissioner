a9ed4bcc36e4d3f83265bdac6a37edcc63bd4cd2
Thea Choem <29684683+theachoem@users.noreply.github.com>
2025-05-14 17:04:39 +0700

    Close #2643 fix passing wrong send_name which cause sms to not send (#2644)

--- app/interactors/spree_cm_commissioner/telegram_debug_pin_code_sender.rb
+++ app/interactors/spree_cm_commissioner/telegram_debug_pin_code_sender.rb
@@ -1,6 +1,6 @@
 module SpreeCmCommissioner
   class TelegramDebugPinCodeSender < BaseInteractor
-    delegate :pin_code, :name, to: :context
+    delegate :pin_code, :name, :error_message, to: :context
 
     def call
       telegram_client.send_message(
@@ -16,6 +16,7 @@ module SpreeCmCommissioner
       text << "<b>From: #{name}</b>"
       text << "<b>PIN CODE sent to #{pin_code.contact}</b>"
       text << "<code>#{pin_code.code}</code> is your #{pin_code.readable_type}"
+      text << "⚠️ Error: <code>#{error_message}<code> ⚠️" if error_message.present?
 
       text.compact.join("\n")
     end
